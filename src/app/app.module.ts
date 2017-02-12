1e3d6942235181714f01d5101f73c27815ea7bc2
akshay <akshay@emaxers.com>
2017-02-12 12:30:26 -0500

    added redux devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,18 @@ import 'hammerjs';
 import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
 import { routes }   from './app.route';
 import { AppComponent, CategoriesComponent, TagsComponent, 
          QuestionsComponent, QuestionAddUpdateComponent } from './components';
 import { CategoryService, TagService, QuestionService } from './services';
 
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-
 import {CategoryActions, TagActions, QuestionActions} from './store/actions';
 import {CategoryEffects, TagEffects, QuestionEffects} from './store/effects';
 import { default as reducer } from './store/app-store';
-
 
 @NgModule({
   declarations: [
@@ -43,6 +43,10 @@ import { default as reducer } from './store/app-store';
     //store
     StoreModule.provideStore(reducer),
     
+    StoreDevtoolsModule.instrumentOnlyWithExtension({
+      maxAge: 20
+    }),
+
     //ngrx effects
     EffectsModule.run(CategoryEffects),
     EffectsModule.run(TagEffects),
