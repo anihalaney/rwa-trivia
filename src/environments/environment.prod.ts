23201ceb4e185fcf7d3c286a4bc4499cb2aabe68
akshay <akshay@emaxers.com>
2017-03-11 17:08:18 -0500

    environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+import { IConfig } from './iconfig';
+
 export const environment = {
   production: true
+};
+
+export const CONFIG: IConfig = {
+  "firebaseConfig" : {
+      apiKey: "AIzaSyDIEpabJv44Iu7go6M30T3WAF-GlSMcR7Y",
+      authDomain: "rwa-trivia.firebaseapp.com",
+      databaseURL: "https://rwa-trivia.firebaseio.com",
+      storageBucket: "rwa-trivia.appspot.com",
+      messagingSenderId: "479350787602"
+  }
 };
