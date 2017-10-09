9f5488ba6a13d13e1c2b8072597125e9469a1d34
akshay <akshay@emaxers.com>
2017-10-09 17:04:10 -0400

    new ui version

--- ui/html/js/custom.js
+++ ui/html/js/custom.js
@@ -1,53 +1,114 @@
 /*Site Navigation Js Start Here*/
+// Load More
+$(document).ready(function () {
+    // Load the first 3 list items from another HTML file
+    //$('#currentgame').load('externalList.html li:lt(3)');
+    $('#currentgame li:lt(4)').show();
+    $('.loadMore').click(function () {
+        // $(this).parent().siblings('ul li:lt(10)').show();
+        $('#currentgame li:lt(10)').show();
+        $(this).hide();
+        $(this).siblings('a').show();
+    });
+    $('.showLess').click(function () {
+        // $(this).parent().siblings('ul li').not(':lt(4)').hide();
+        $('#currentgame li').not(':lt(4)').hide();
+        $(this).siblings('a').show();
+        $(this).hide();
+    });
+});
+
+$(document).ready(function () {    
+    $('.invitationlist li:lt(3)').show();
+    $('.loadMore2').click(function () {       
+        $('.invitationlist li:lt(10)').show();
+        $(this).hide();
+        $(this).siblings('a').show();
+    });
+    $('.showLess2').click(function () {       
+        $('.invitationlist li').not(':lt(3)').hide();
+        $(this).siblings('a').show();
+        $(this).hide();
+    });
+});
+$(document).ready(function () {    
+    $('.categorybox:lt(3)').show();
+    $('.loadMore3').click(function () {       
+        $('.categorybox:lt(10)').show();
+        $(this).hide();
+        $(this).siblings('a').show();
+    });
+    $('.showLess3').click(function () {       
+        $('.categorybox').not(':lt(3)').hide();
+        $(this).siblings('a').show();
+        $(this).hide();
+    });
+});
+
+// $(document).ready(function () {
+//     // Load the first 3 list items from another HTML file
+//     //$('#currentgame').load('externalList.html li:lt(3)');
+//     $('.invitationlist li:lt(3)').show();
+//     $('.loadMore').click(function () {
+//         $('.invitationlist li:lt(10)').show();
+//         $('.loadMore').hide();
+//         $('.showLess').show();
+//     });
+//     $('.showLess').click(function () {
+//         $('.invitationlist li').not(':lt(3)').hide();
+//         $('.loadMore').show();
+//         $('.showLess').hide();
+//     });
+// });
 
 jQuery(document).ready(function($){
 
-  if (screen.width < 1025) {
-    $('#currentgame ul').owlCarousel({
-        loop:false,
-        autoPlay: false,
-        margin:10,
-        pagination:false,
-        autoWidth:true,
-        itemsTablet : [768, 2.5],
-        itemsDesktop : [1024, 3.2],
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-            }
-        }
-    })
-}  
+//   if (screen.width < 1025) {
+//     $('#currentgame ul').owlCarousel({
+//         loop:false,
+//         autoPlay: false,
+//         margin:10,
+//         pagination:false,
+//         autoWidth:true,
+//         itemsTablet : [768, 2.5],
+//         itemsDesktop : [1024, 3.2],
+//         responsive:{
+//             0:{
+//                 items:1,
+//             },
+//             600:{
+//                 items:2,
+//                 nav:false
+//             },
+//             1000:{
+//                 items:3,
+//             }
+//         }
+//     })
+// }  
 
-if (screen.width < 1023) {
-    $('.invitationlist').owlCarousel({
-        loop:false,
-        autoPlay: false,
-        margin:10,
-        pagination:false,
-        autoWidth:true,
-        itemsTablet : [768, 2.5],
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-            }
-        }
-    })
-}
+// if (screen.width < 1023) {
+//     $('.invitationlist').owlCarousel({
+//         loop:false,
+//         autoPlay: false,
+//         margin:10,
+//         pagination:false,
+//         autoWidth:true,
+//         itemsTablet : [768, 2.5],
+//         responsive:{
+//             0:{
+//                 items:1,
+//             },
+//             600:{
+//                 items:2,
+//                 nav:false
+//             },
+//             1000:{
+//                 items:3,
+//             }
+//         }
+//     })
+// }
 
 // if (screen.width < 1024) {
 //     $('.blog').owlCarousel({
