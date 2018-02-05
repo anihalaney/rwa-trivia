ef351bef6ea180d3ccf9e9963b35af10a0e13dd2
Milan Saraiya <milan@ashutec.com>
2018-02-05 16:03:47 +0530

    code changes after code review

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -5,7 +5,7 @@ import { MatSnackBar } from '@angular/material';
 import { Subscription } from 'rxjs/Subscription';
 
 import { AppStore } from '../../core/store/app-store';
-import { CategoryActions, TagActions, QuestionActions, GameActions, BulkUploadActions } from '../../core/store/actions';
+import { CategoryActions, TagActions, QuestionActions, GameActions} from '../../core/store/actions';
 import { AuthenticationService, Utils } from '../../core/services';
 import { User } from '../../model';
 
@@ -26,7 +26,6 @@ export class AppComponent implements OnInit, OnDestroy {
               private categoryActions: CategoryActions,
               private tagActions: TagActions,
               private questionActions: QuestionActions,
-              private bulkUploadActions: BulkUploadActions,
               private gameActions: GameActions,
               private store: Store<AppStore>,
               public router: Router,
@@ -60,8 +59,7 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnInit () {
     console.log("dispatch");
     this.store.dispatch(this.categoryActions.loadCategories());
-    this.store.dispatch(this.tagActions.loadTags());
-    this.store.dispatch(this.bulkUploadActions.loadFileRecord());
+    this.store.dispatch(this.tagActions.loadTags());    
   }
 
   ngOnDestroy() {
