baf8fe8e87c173421340512110f1dc87ffc08d07
yugank1991 <concent08@gmail.com>
2018-02-07 17:35:11 +0530

    Done Changes related to display file details in bulk upload summary at client side

--- src/app/bulk/bulk.module.ts
+++ src/app/bulk/bulk.module.ts
@@ -4,12 +4,13 @@ import { SharedModule } from  '../shared/shared.module';
 import { BulkRoutingModule } from  './routing/bulk-routing.module';
 
 import { BulkSummaryComponent, BulkDetailsComponent, BulkUploadComponent } from './components';
+import { BulkSummaryQuestionListComponent } from './components/bulk-summary/bulk-summary-question-list/bulk-summary-question-list.component';
 
 @NgModule({
   declarations: [
     BulkSummaryComponent, 
     BulkDetailsComponent, 
-    BulkUploadComponent
+    BulkUploadComponent, BulkSummaryQuestionListComponent
   ],
   imports: [
     //rwa modules
