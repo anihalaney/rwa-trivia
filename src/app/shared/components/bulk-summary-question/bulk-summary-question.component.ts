b5d7dbba95db038e31d10e236c92cbeeecdfe61b
yugank <yugank@montpeliergrp.com>
2018-02-09 20:22:34 +0530

    Done changes related to show buttons only in unpublished question area

--- src/app/shared/components/bulk-summary-question/bulk-summary-question.component.ts
+++ src/app/shared/components/bulk-summary-question/bulk-summary-question.component.ts
@@ -3,20 +3,21 @@ import { DataSource } from '@angular/cdk/table';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Store } from '@ngrx/store';
-import { AppStore } from '../../../../core/store/app-store';
-import { BulkUploadFileInfo, Question, Category, User } from '../../../../model';
-import { BulkUploadActions, QuestionActions } from '../../../../core/store/actions';
+import { AppStore } from '../../../core/store/app-store';
+import { BulkUploadFileInfo, Question, Category, User } from '../../../model';
+import { BulkUploadActions, QuestionActions } from '../../../core/store/actions';
 import { bulkUploadPublishedQuestions, bulkUploadUnpublishedQuestions, bulkUploadFileInfosById } from 'app/core/store/reducers';
 import { Subscription } from 'rxjs/Subscription';
 import { Router, ActivatedRoute } from '@angular/router';
-import {Location} from '@angular/common';
+import { Location } from '@angular/common';
+
 
 @Component({
-  selector: 'app-bulk-summary-question-list',
-  templateUrl: './bulk-summary-question-list.component.html',
-  styleUrls: ['./bulk-summary-question-list.component.scss']
+  selector: 'app-bulk-summary-questions',
+  templateUrl: './bulk-summary-question.component.html',
+  styleUrls: ['./bulk-summary-question.component.scss']
 })
-export class BulkSummaryQuestionListComponent implements OnInit, OnChanges {
+export class BulkSummaryQuestionComponent implements OnInit, OnChanges {
 
   unPublishedQuestions: Question[];
   publishedQuestions: Question[];
@@ -41,7 +42,14 @@ export class BulkSummaryQuestionListComponent implements OnInit, OnChanges {
   uploadsDS: FileUploadsDataSource;
   bulkUploadObs: Observable<BulkUploadFileInfo>;
   uploadsSubject: BehaviorSubject<BulkUploadFileInfo[]>;
-  uploadsSubjectArray: Array<BulkUploadFileInfo>;
+  uploadsSubjectArray: Array<BulkUploadFileInfo> = [];
+  PUBLISHED_SHOW_BUTTON_STATE = false;
+  UNPUBLISHED_SHOW_BUTTON_STATE = true;
+
+  @Input() bulkUploadFileInfo: BulkUploadFileInfo;
+  @Input() isAdminUrl: boolean;
+
+
 
   constructor(private store: Store<AppStore>,
     public router: Router,
@@ -53,46 +61,6 @@ export class BulkSummaryQuestionListComponent implements OnInit, OnChanges {
     this.uploadsSubject = new BehaviorSubject<BulkUploadFileInfo[]>([]);
     this.uploadsDS = new FileUploadsDataSource(this.uploadsSubject);
 
-    this.route.params.subscribe((params) => {
-
-      this.fileId = params['id'];
-      const bulkUploadFileInfoObject = new BulkUploadFileInfo();
-      bulkUploadFileInfoObject.id = this.fileId;
-
-      this.unPublishedQuestionObs = store.select(s => s.bulkUploadUnpublishedQuestions);
-      this.publishedQuestionObs = store.select(s => s.bulkUploadPublishedQuestions);
-      this.bulkUploadObs = store.select(s => s.bulkUploadFileInfosById);
-
-      // get the bulk Upload Recored by Id
-      this.store.dispatch(this.bulkUploadAction.loadBulkUploadById(bulkUploadFileInfoObject));
-      this.bulkSubs = this.bulkUploadObs.subscribe((userBulkUploadFileInfo) => {
-
-        this.uploadsSubjectArray = [];
-        this.userBulkUploadFileInfo = userBulkUploadFileInfo;
-
-        if (userBulkUploadFileInfo !== undefined) {
-          this.uploadsSubjectArray.push(userBulkUploadFileInfo[0]);
-          this.uploadsSubject.next(this.uploadsSubjectArray);
-        }
-      });
-
-      // get published question by BulkUpload Id
-      this.store.dispatch(this.questionActions.loadBulkUploadPublishedQuestions(bulkUploadFileInfoObject));
-      this.publishedSub = this.publishedQuestionObs.subscribe((questions) => {
-        this.publishedCount = questions.length;
-        this.publishedQuestions = questions;
-        // console.log('published', this.publishedQuestions);
-      });
-
-      // get unpublished question by BulkUpload Id
-      this.store.dispatch(this.questionActions.loadBulkUploadUnpublishedQuestions(bulkUploadFileInfoObject));
-      this.unPublishedSub = this.unPublishedQuestionObs.subscribe((questions) => {
-        this.unPublishedCount = questions.length;
-        this.unPublishedQuestions = questions;
-        // console.log('unPublishedQuestions', this.unPublishedQuestions);
-      });
-
-    });
 
   }
 
@@ -102,6 +70,30 @@ export class BulkSummaryQuestionListComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {
+    if (this.bulkUploadFileInfo) {
+      this.uploadsSubjectArray.push(this.bulkUploadFileInfo);
+      this.uploadsSubject.next(this.uploadsSubjectArray);
+
+      this.unPublishedQuestionObs = this.store.select(s => s.bulkUploadUnpublishedQuestions);
+      this.publishedQuestionObs = this.store.select(s => s.bulkUploadPublishedQuestions);
+
+      // get published question by BulkUpload Id
+      this.store.dispatch(this.questionActions.loadBulkUploadPublishedQuestions(this.bulkUploadFileInfo));
+      this.publishedSub = this.publishedQuestionObs.subscribe((questions) => {
+        this.publishedCount = questions.length;
+        this.publishedQuestions = questions;
+        // console.log('published', this.publishedQuestions);
+      });
+
+      // get unpublished question by BulkUpload Id
+      this.store.dispatch(this.questionActions.loadBulkUploadUnpublishedQuestions(this.bulkUploadFileInfo));
+      this.unPublishedSub = this.unPublishedQuestionObs.subscribe((questions) => {
+        this.unPublishedCount = questions.length;
+        this.unPublishedQuestions = questions;
+        // console.log('unPublishedQuestions', this.unPublishedQuestions);
+      });
+
+    }
   }
 
   // approveQuestions
@@ -112,9 +104,7 @@ export class BulkSummaryQuestionListComponent implements OnInit, OnChanges {
   //   this.store.dispatch(this.questionActions.approveQuestion(question));
   // }
 
-  backToSummary() {
-    this._location.back();
-  }
+
 
 }
 
