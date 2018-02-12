ad99b1102cd8efa745f0e5a267d38fc4298da79c
akshay <akshay@emaxers.com>
2018-02-12 07:13:25 -0500

    interim commit

---
+++ src/app/store/reducers/index.ts
@@ -0,0 +1,39 @@
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Params } from '@angular/router';
+
+import * as fromRouter from '@ngrx/router-store';
+import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
+import { RouterStateSerializer } from '@ngrx/router-store/src/serializer';
+
+export interface RouterStateUrl {
+  url: string;
+  queryParams: Params;
+  params: Params;
+}
+export interface State {
+  routerReducer: fromRouter.RouterReducerState<RouterStateUrl>
+}
+
+export const reducers: ActionReducerMap<State> = {
+  routerReducer: fromRouter.routerReducer
+}
+
+export const routerState = 
+  createFeatureSelector<fromRouter.RouterReducerState<RouterStateUrl>>('routerReducer');
+
+export class CustomSerializer implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const {url} = routerState;
+    const {queryParams} = routerState.root;
+
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      state = state.firstChild;
+
+    }
+
+    const {params} = state;
+
+    return { url, queryParams, params };
+  }
+}
