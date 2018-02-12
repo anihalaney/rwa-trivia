63d875001983dc75bc3a94cf875f20f1b19079ef
Yugank Choksi <concent08@gmail.com>
2018-02-12 17:05:43 +0530

    Done changes related to implement functionality of reject, request to change  and edit question button

--- src/app/shared/components/questions-table/questions-table.component.ts
+++ src/app/shared/components/questions-table/questions-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, Output, OnInit, OnChanges, EventEmitter } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, Validators, FormArray, FormControl, ValidatorFn } from '@angular/forms';
 import { DataSource } from '@angular/cdk/table';
 import { PageEvent, MatCheckboxChange, MatSelectChange } from '@angular/material';
 import { Store } from '@ngrx/store';
@@ -9,8 +9,10 @@ import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
-import { Question, QuestionStatus, Category, User } from '../../../model';
+import { Question, QuestionStatus, Category, User, Answer } from '../../../model';
 import { QuestionActions } from '../../../core/store/actions';
+
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'question-table',
@@ -42,6 +44,7 @@ export class QuestionsTableComponent implements OnInit, OnChanges {
 
   requestQuestionStatus = false;
   rejectQuestionStatus = false;
+  editQuestionStatus = false;
   emptyReason = false;
 
 
@@ -49,6 +52,36 @@ export class QuestionsTableComponent implements OnInit, OnChanges {
 
   requestQuestion: Question;
   rejectQuestion: Question;
+  editQuestion: Question;
+  // question: Question;
+
+
+
+  // for edit
+
+  tagsObs: Observable<string[]>;
+  categoriesObs: Observable<Category[]>;
+
+  subs: Subscription[] = [];
+
+  // Properties
+  categories: Category[];
+  tags: string[];
+
+  questionForm: FormGroup;
+  question: Question;
+
+  autoTags: string[] = []; // auto computed based on match within Q/A
+  enteredTags: string[] = [];
+
+  user: User;
+
+  get answers(): FormArray {
+    return this.questionForm.get('answers') as FormArray;
+  }
+  get tagsArray(): FormArray {
+    return this.questionForm.get('tagsArray') as FormArray;
+  }
 
   constructor(private store: Store<AppStore>,
     private questionActions: QuestionActions,
@@ -56,6 +89,18 @@ export class QuestionsTableComponent implements OnInit, OnChanges {
     this.questionsSubject = new BehaviorSubject<Question[]>([]);
     this.questionsDS = new QuestionsDataSource(this.questionsSubject);
     this.sortOrder = 'Category';
+
+
+    this.categoriesObs = store.select(s => s.categories);
+    this.tagsObs = store.select(s => s.tags);
+
+
+    const question = new Question();
+
+    this.questionForm = this.fb.group({
+      category: [(question.categories.length > 0 ? question.categories[0] : ''), Validators.required]
+    });
+
   }
 
   ngOnInit() {
@@ -65,6 +110,19 @@ export class QuestionsTableComponent implements OnInit, OnChanges {
     this.rejectFormGroup = this.fb.group({
       reason: ['', Validators.required]
     });
+
+    // edit question
+    this.question = new Question();
+    this.createForm(this.question);
+
+    let questionControl = this.questionForm.get('questionText');
+
+    questionControl.valueChanges.debounceTime(500).subscribe(v => this.computeAutoTags());
+    this.answers.valueChanges.debounceTime(500).subscribe(v => this.computeAutoTags());
+
+    this.subs.push(this.categoriesObs.subscribe(categories => this.categories = categories));
+    this.subs.push(this.tagsObs.subscribe(tags => this.tags = tags));
+
   }
 
   ngOnChanges() {
@@ -83,6 +141,7 @@ export class QuestionsTableComponent implements OnInit, OnChanges {
     question.approved_uid = user.userId;
     this.store.dispatch(this.questionActions.approveQuestion(question));
   }
+
   displayRequestToChange(question: Question) {
     this.requestQuestionStatus = true;
     this.rejectQuestionStatus = false;
@@ -101,7 +160,8 @@ export class QuestionsTableComponent implements OnInit, OnChanges {
     }
     this.requestQuestion.status = QuestionStatus.REQUEST_TO_CHANGE;
     this.requestQuestion.reason = this.requestFormGroup.get('reason').value;
-    this.store.dispatch(this.questionActions.addQuestion(this.requestQuestion));
+    this.requestQuestionStatus = false;
+    this.store.dispatch(this.questionActions.updateQuestion(this.requestQuestion));
   }
   saveRejectToChangeQuestion() {
     if (!this.rejectFormGroup.valid) {
@@ -109,7 +169,127 @@ export class QuestionsTableComponent implements OnInit, OnChanges {
     }
     this.rejectQuestion.status = QuestionStatus.REJECTED;
     this.rejectQuestion.reason = this.rejectFormGroup.get('reason').value;
-    this.store.dispatch(this.questionActions.addQuestion(this.rejectQuestion));
+    this.rejectQuestionStatus = false;
+    this.store.dispatch(this.questionActions.updateQuestion(this.rejectQuestion));
+  }
+
+
+  editQuestions(row: Question) {
+    this.editQuestion = row;
+    this.questionForm.controls['category'].setValue(row.categoryIds);
+    this.questionForm.controls['questionText'].setValue(row.questionText);
+    // this.questionForm.controls['answers']['control'].setValue(row.answers);
+    this.enteredTags = row.tags;
+    console.log(this.enteredTags);
+  }
+
+
+
+
+  // Event Handlers
+  addTag() {
+    let tag = this.questionForm.get('tags').value;
+    if (tag) {
+      if (this.enteredTags.indexOf(tag) < 0)
+        this.enteredTags.push(tag);
+      this.questionForm.get('tags').setValue('');
+    }
+    this.setTagsArray();
+  }
+  removeEnteredTag(tag) {
+    this.enteredTags = this.enteredTags.filter(t => t !== tag);
+    this.setTagsArray();
+  }
+  onSubmit() {
+    // validations
+    this.questionForm.updateValueAndValidity();
+    if (this.questionForm.invalid)
+      return;
+
+    // get question object from the forms
+    // console.log(this.questionForm.value);
+    let question: Question = this.getQuestionFromFormValue(this.questionForm.value);
+
+    question.status = QuestionStatus.SUBMITTED;
+    this.store.take(1).subscribe(s => this.user = s.user);
+    // console.log(question);
+
+    question.created_uid = this.user.userId;
+    // call saveQuestion
+    this.saveQuestion(question);
+  }
+
+  // Helper functions
+  getQuestionFromFormValue(formValue: any): Question {
+    let question: Question;
+
+    question = new Question();
+    question.questionText = formValue.questionText;
+    question.answers = formValue.answers;
+    question.categoryIds = [formValue.category];
+    question.tags = [...this.autoTags, ...this.enteredTags]
+    question.ordered = formValue.ordered;
+    question.explanation = formValue.explanation;
+
+    return question;
+  }
+
+  saveQuestion(question: Question) {
+    console.log('question--->', JSON.stringify(question));
+    this.store.dispatch(this.questionActions.addQuestion(question));
+  }
+
+  computeAutoTags() {
+    let formValue = this.questionForm.value;
+
+    let allTextValues: string[] = [formValue.questionText];
+    formValue.answers.forEach(answer => allTextValues.push(answer.answerText));
+
+    let wordString: string = allTextValues.join(" ");
+
+    let matchingTags: string[] = [];
+    this.tags.forEach(tag => {
+      let patt = new RegExp('\\b(' + tag.replace("+", "\\+") + ')\\b', "ig");
+      if (wordString.match(patt))
+        matchingTags.push(tag);
+    });
+    this.autoTags = matchingTags;
+
+    this.setTagsArray();
+  }
+  setTagsArray() {
+    this.tagsArray.controls = [];
+    [...this.autoTags, ...this.enteredTags].forEach(tag => this.tagsArray.push(new FormControl(tag)));
+  }
+  createForm(question: Question) {
+
+    let fgs: FormGroup[] = question.answers.map(answer => {
+      let fg = new FormGroup({
+        answerText: new FormControl(answer.answerText, Validators.required),
+        correct: new FormControl(answer.correct),
+      });
+      return fg;
+    });
+    let answersFA = new FormArray(fgs);
+
+    let fcs: FormControl[] = question.tags.map(tag => {
+      let fc = new FormControl(tag);
+      return fc;
+    });
+    if (fcs.length == 0)
+      fcs = [new FormControl('')];
+    let tagsFA = new FormArray(fcs);
+
+    this.questionForm = this.fb.group({
+      category: [(question.categories.length > 0 ? question.categories[0] : ''), Validators.required],
+      questionText: [question.questionText, Validators.required],
+      tags: '',
+      tagsArray: tagsFA,
+      answers: answersFA,
+      ordered: [question.ordered],
+      explanation: [question.explanation]
+    }, { validator: questionFormValidator }
+    );
   }
 
   approveButtonClicked(question: Question) {
@@ -134,4 +314,20 @@ export class QuestionsDataSource extends DataSource<Question> {
   }
 
   disconnect() { }
+}
+
+
+
+// Custom Validators
+function questionFormValidator(fg: FormGroup): { [key: string]: boolean } {
+  let answers: Answer[] = fg.get('answers').value;
+  if (answers.filter(answer => answer.correct).length !== 1) {
+    return { 'correctAnswerCountInvalid': true }
+  }
+
+  let tags: string[] = fg.get('tagsArray').value;
+  if (tags.length < 3)
+    return { 'tagCountInvalid': true }
+
+  return null;
 }
