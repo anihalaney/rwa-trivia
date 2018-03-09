0ff475c1c255656e7d7d57bfd639093d760957cf
Milan Saraiya <milan@ashutec.com>
2018-03-09 18:18:40 +0530

    add security to function

--- functions/app.ts
+++ functions/app.ts
@@ -226,25 +226,25 @@ app.get('/migrate_to_firestore/:collection', adminOnly, (req, res) => {
 
 });
 
-app.get('/migrate_data_from_prod_dev/:collection', (req, res) => {
+app.get('/migrate_data_from_prod_dev/:collection', adminOnly, (req, res) => {
 
   // Initialize another app
-  const otherApp = admin.initializeApp({
-    apiKey: "AIzaSyDIEpabJv44Iu7go6M30T3WAF-GlSMcR7Y",
-    authDomain: "rwa-trivia.firebaseapp.com",
-    databaseURL: "https://rwa-trivia.firebaseio.com",
+  const sourceApp = admin.initializeApp({
+    apiKey: 'AIzaSyDIEpabJv44Iu7go6M30T3WAF-GlSMcR7Y',
+    authDomain: 'rwa-trivia.firebaseapp.com',
+    databaseURL: 'https://rwa-trivia.firebaseio.com',
     projectId: 'rwa-trivia',
-    storageBucket: "rwa-trivia.appspot.com",
-    messagingSenderId: "479350787602"
-  }, "otherApp");
+    storageBucket: 'rwa-trivia.appspot.com',
+    messagingSenderId: '479350787602'
+  }, 'sourceApp');
   console.log(req.params.collection);
-  const db = otherApp.firestore();
-  const otherDb = admin.firestore();
-  db.collection(req.params.collection).get()
+  const sourceDB = sourceApp.firestore();
+  const targetDB = admin.firestore();
+  sourceDB.collection(req.params.collection).get()
     .then((snapshot) => {
       snapshot.forEach((doc) => {
         console.log(doc.id, '=>', doc.data());
-        otherDb.collection(req.params.collection).doc(doc.id).set(doc.data());
+        targetDB.collection(req.params.collection).doc(doc.id).set(doc.data());
       });
       res.send('loaded data');
     })
