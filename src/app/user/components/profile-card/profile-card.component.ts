89893d987921865764c63af277f114dfc61118ef
saloniss1345 <saloniss1345@gmail.com>
2018-03-20 18:02:57 +0530

    Started working on displaying user profile image.

--- src/app/user/components/profile-card/profile-card.component.ts
+++ src/app/user/components/profile-card/profile-card.component.ts
@@ -1,12 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 import { User } from '../../../model';
+import { Store } from '@ngrx/store';
+import { AppState, appState, categoryDictionary, getCategories, getTags } from '../../../store';
+import * as userActions from '../../store/actions';
+import { Observable } from 'rxjs/Observable';
+import { userState } from '../../store';
 
 @Component({
   selector: 'profile-card',
   templateUrl: './profile-card.component.html',
   styleUrls: ['./profile-card.component.scss']
 })
-export class ProfileCardComponent {
+export class ProfileCardComponent implements OnChanges {
   @Input() user: User;
+  userObs: Observable<User>;
+
+  constructor(private store: Store<AppState>) {
+    this.userObs = this.store.select(userState).select(s => s.user);
+
+    this.userObs.subscribe(user => {
+      if (user !== null) {
+        this.user = user;
+      }
+    });
+
+  }
+
+  ngOnChanges() {
+    if (this.user) {
+      this.store.dispatch(new userActions.LoadUserProfile({ user: this.user }));
+
+    }
+  }
 }
