b0c7788c2a227b8443b4a2b1bf7b932bcad89078
saloniss1345 <saloniss1345@gmail.com>
2018-03-29 18:43:36 +0530

    Remove unused code form user service.

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { AngularFireStorage } from 'angularfire2/storage';
 import { Observable } from 'rxjs/Observable';
 import '../../rxjs-extensions';
-import { CONFIG } from '../../../environments/environment';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app-store';
-import { UserActions } from '../store/actions';
 import { User } from '../../model';
 import * as useractions from '../../user/store/actions';
-import { useAnimation } from '@angular/core/src/animation/dsl';
 
 
 
@@ -19,9 +15,7 @@ export class UserService {
 
     constructor(private db: AngularFirestore,
         private storage: AngularFireStorage,
-        private store: Store<AppState>,
-        private userActions: UserActions,
-        private http: HttpClient) {
+        private store: Store<AppState>) {
     }
 
 
