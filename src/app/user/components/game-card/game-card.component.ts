5663517e297c3396f71ea12bd9596e83a6e63888
Yugank Choksi <concent08@gmail.com>
2018-03-29 18:33:30 +0530

    Removed unused code

--- src/app/user/components/game-card/game-card.component.ts
+++ src/app/user/components/game-card/game-card.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Store } from '@ngrx/store';
-
 import { AppState, appState } from '../../../store';
 import { User, Game } from '../../../model';
 import { userState } from '../../store';
@@ -13,26 +12,20 @@ import { userState } from '../../store';
 })
 export class GameCardComponent implements OnInit, OnChanges {
   @Input() game: Game;
-  userObs: Observable<User>;
+  userObs$: Observable<User>;
   correctAnswerCount: number;
   questionIndex: number;
-  userProfilePicUrl: string;
   user: User;
   myTurn: boolean;
-  location: string;
+
 
   constructor(private store: Store<AppState>) {
-    this.userProfilePicUrl = '/assets/images/yourimg.png';
-    this.userObs = this.store.select(userState).select(s => s.user);
 
-    this.userObs.subscribe(user => {
+    this.userObs$ = this.store.select(userState).select(s => s.user);
+
+    this.userObs$.subscribe(user => {
       if (user !== null) {
         this.user = user;
-        console.log("user" + JSON.stringify(this.user.location));
-        if (this.user.profilePictureUrl) {
-          this.location = this.user.location;
-          this.user.profilePictureUrl.subscribe(url => this.userProfilePicUrl = url);
-        }
       }
     });
   }
