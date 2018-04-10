6a644cb16cd895e5b35f7fcfa0aa45ae67caed45
saloniss1345 <saloniss1345@gmail.com>
2018-04-10 19:03:41 +0530

    Created model ui and validate multiple email address.

--- src/app/user/components/invite-friends/invite-friends-dialog/invite-mail-friends/invite-mail-friends.component.ts
+++ src/app/user/components/invite-friends/invite-friends-dialog/invite-mail-friends/invite-mail-friends.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
 import { User } from '../../../../../model';
 
+const EMAIL_REGEXP = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 @Component({
   selector: 'app-invite-mail-friends',
   templateUrl: './invite-mail-friends.component.html',
@@ -10,19 +12,55 @@ import { User } from '../../../../../model';
 export class InviteMailFriendsComponent implements OnInit {
   @Input() user: User;
   invitationForm: FormGroup;
+  showErrorMsg = false;
+  invalidEmailList = [];
+  errorMsg = '';
+
   constructor(private fb: FormBuilder) {
-    this.createForm();
   }
 
   ngOnInit() {
-  }
-
-  // create the form
-  createForm() {
     this.invitationForm = this.fb.group({
-      email: '',
-
+      email: ['', Validators.compose([Validators.required])]
     });
   }
 
+  isValid(email) {
+    return EMAIL_REGEXP.test(String(email).toLowerCase());
+  }
+
+
+  onSubscribe() {
+    if (!this.invitationForm.valid) {
+      return;
+    } else {
+      let invalid = false;
+      this.errorMsg = '';
+      this.showErrorMsg = false;
+      this.invalidEmailList = [];
+
+      if (this.invitationForm.get('email').value.indexOf(',') > -1) {
+        const emails = this.invitationForm.get('email').value.split(',');
+        for (const e of emails) {
+          invalid = this.isValid(e);
+          if (!invalid) {
+            this.invalidEmailList.push(e);
+          }
+        }
+        if (this.invalidEmailList.length > 0) {
+          this.errorMsg = 'Following emails are not valid address!';
+          this.showErrorMsg = true;
+        }
+      } else {
+        const email = this.invitationForm.get('email').value.split(',');
+        if (email === '' || !this.isValid(email)) {
+          invalid = true;
+          this.invalidEmailList.push(email);
+          this.errorMsg = 'Following email is not valid address!';
+          this.showErrorMsg = true;
+        }
+      }
+    }
+  }
 }
+
