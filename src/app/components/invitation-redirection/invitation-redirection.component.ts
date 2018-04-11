0993a1ad8aede88bdd9a15bf2bdfdb8bbca6bc05
saloniss1345 <saloniss1345@gmail.com>
2018-04-11 17:45:15 +0530

    Get token from url.

--- src/app/components/invitation-redirection/invitation-redirection.component.ts
+++ src/app/components/invitation-redirection/invitation-redirection.component.ts
@@ -1,10 +1,26 @@
 import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
     selector: 'invitation-redirection',
     templateUrl: './invitation-redirection.component.html',
     styleUrls: ['./invitation-redirection.component.scss', './invitation-redirection.component.scss']
 })
-export class InvitationRedirectionComponent {
+export class InvitationRedirectionComponent implements OnInit {
+
+    constructor(private activatedRoute: ActivatedRoute, private router: Router) {
+
+    }
+
+    ngOnInit() {
+        // subscribe to router event
+        this.activatedRoute.params.subscribe((params: Params) => {
+            const token = params['token'];
+            setTimeout(() => {
+                this.router.navigate(['/dashboard']);
+            }, 3000);
+
+        });
+    }
 
 }
