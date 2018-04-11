6429756eacfa1824fb1e4dcb88604cbf17c2cc56
saloniss1345 <saloniss1345@gmail.com>
2018-04-11 16:12:13 +0530

    Created invitation-redirection component.

---
+++ src/app/components/invitation-redirection/invitation-redirection.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+
+@Component({
+    selector: 'invitation-redirection',
+    templateUrl: './invitation-redirection.component.html',
+    styleUrls: ['./invitation-redirection.component.scss', './invitation-redirection.component.scss']
+})
+export class InvitationRedirectionComponent {
+
+}
