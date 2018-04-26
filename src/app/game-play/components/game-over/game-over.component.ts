da550b856722e38dd8121e4ce091f34c85e783a5
saloniss1345 <saloniss1345@gmail.com>
2018-04-26 18:26:40 +0530

    Created Ui for game over dialog box .

--- src/app/game-play/components/game-over/game-over.component.ts
+++ src/app/game-play/components/game-over/game-over.component.ts
@@ -1,17 +1,43 @@
 import { Component, Input, Output, OnInit, OnDestroy, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+import { User, Game } from '../../../model';
+import { Observable } from 'rxjs/Observable';
+import { Store } from '@ngrx/store';
+import { AppState, appState } from '../../../store';
 
 @Component({
   selector: 'game-over',
   templateUrl: './game-over.component.html',
   styleUrls: ['./game-over.component.scss']
 })
-export class GameOverComponent  {
+export class GameOverComponent implements OnInit {
   @Input() correctCount: number;
   @Input() noOfQuestions: number;
   @Output() gameOverContinueClicked = new EventEmitter();
   @Output() viewQuestionClicked = new EventEmitter<any>();
+  @Input() categoryName: string;
+  @Input() game: Game;
+  @Input() userDict: { [key: string]: User };
+  user$: Observable<User>;
+  user: User;
+  otherUserId: string;
+  otherUserInfo: User;
 
   continueButtonClicked(event: any) {
     this.gameOverContinueClicked.emit();
+  }
+
+  constructor(private store: Store<AppState>) {
+    this.user$ = this.store.select(appState.coreState).select(s => s.user);
+    this.user$.subscribe(user => {
+      if (user !== null) {
+        this.user = user;
+      }
+    });
+  }
+  ngOnInit() {
+    if (this.game) {
+      this.otherUserId = this.game.playerIds.filter(userId => userId !== this.user.userId)[0];
+      this.otherUserInfo = this.userDict[this.otherUserId];
+    }
   }
 }
