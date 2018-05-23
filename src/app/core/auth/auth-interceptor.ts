486754fad57db33b408a431b4baf548649a2e90d
yugank <yugank.choksi@sooryen.com>
2018-05-23 20:47:10 +0530

    removed unused code

--- src/app/core/auth/auth-interceptor.ts
+++ src/app/core/auth/auth-interceptor.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
 import { Store } from '@ngrx/store';
 
 import { AppState } from '../../store';
