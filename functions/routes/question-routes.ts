371999753b241e6005863582c6e50b2af67497c9
yugank <yugank.choksi@sooryen.com>
2018-06-06 16:19:26 +0530

    Done changes related to restrict question of day for 24 hours

--- functions/routes/question-routes.ts
+++ functions/routes/question-routes.ts
@@ -8,7 +8,7 @@ const questionAuth = require('../middlewares/auth');
 const questionController = require('../controllers/question.controller');
 
 
-router.get('/day', questionController.getQuestionOfDay);
+router.get('/day/:nextQ', questionController.getQuestionOfDay);
 router.get('/next/:gameId', questionAuth.authorizedOnly, questionController.getNextQuestion);
 router.get('/game/:gameId', questionAuth.authorizedOnly, questionController.getQuestions);
 router.post('/:start/:size', questionAuth.adminOnly, questionController.getQuestions);
