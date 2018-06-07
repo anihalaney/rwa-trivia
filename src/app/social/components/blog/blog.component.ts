7cde766e854ceb7a8de27cec70504c96a8925378
yugank <yugank.choksi@sooryen.com>
2018-06-07 18:48:23 +0530

    Done changes related to create latest blogs from feed

--- src/app/social/components/blog/blog.component.ts
+++ src/app/social/components/blog/blog.component.ts
@@ -13,21 +13,17 @@ export class BlogComponent implements OnDestroy {
   @Input() blogId: number;
   sub: Subscription;
   blogData = [];
-  blogStartIndex: number;
-  blogLastIndex: number;
 
   constructor(private store: Store<AppState>) {
     this.sub = this.store.select(appState.socialState).select(s => s.blogs).subscribe(blogs => {
       if (blogs.length > 0) {
         this.blogData = blogs;
-        this.blogStartIndex = Math.floor((Math.random() * (this.blogData.length - 3)) + 1);
-        this.blogLastIndex = this.blogStartIndex + 3;
       }
     });
   }
 
   onNotify(info: any) {
-    this.blogData[info.blogNo].share_status = info.share_status;
+    this.blogData[this.blogData.findIndex(blog => blog.blogNo === info.blogNo)].share_status = info.share_status;
   }
 
   ngOnDestroy() {
