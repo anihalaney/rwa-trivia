51b7203eae4f206d4e8a9b820696ec1b068d5023
yugank <yugank.choksi@sooryen.com>
2018-06-14 16:28:12 +0530

    changed scheduler interval

--- scheduler/constants.ts
+++ scheduler/constants.ts
@@ -1,6 +1,6 @@
 export enum schedulerConstants {
     cronExpression = '*/1 * * * *', // every 1 hour
-    turnCronExpression = '* */1 * * *', // every 5 minute
+    turnCronExpression = '* */5 * * *', // every 5 minute
     extensionName = '.cloudfunctions.net',
     domainZone = 'us-central1',
     port = 443,
