61d329b2e061f40154b0953efb22dd5d35699e16
saloniss1345 <saloniss1345@gmail.com>
2018-06-18 12:38:50 +0530

    Added condition to bind proper userAnswerId field value in game.

--- src/app/game-play/components/game-dialog/game-dialog.component.ts
+++ src/app/game-play/components/game-dialog/game-dialog.component.ts
@@ -302,6 +302,12 @@ export class GameDialogComponent implements OnInit, OnDestroy {
   afterAnswer(userAnswerId?: number) {
     Utils.unsubscribe([this.timerSub, this.questionSub]);
     const correctAnswerId = this.currentQuestion.answers.findIndex(a => a.correct);
+    let index;
+    if (userAnswerId === undefined) {
+      index = null;
+    } else {
+      index = userAnswerId.toString();
+    }
 
     if (userAnswerId === correctAnswerId) {
       this.isCorrectAnswer = true;
@@ -311,7 +317,8 @@ export class GameDialogComponent implements OnInit, OnDestroy {
     const seconds = this.MAX_TIME_IN_SECONDS - this.timer;
     const playerQnA: PlayerQnA = {
       playerId: this.user.userId,
-      playerAnswerId: isNaN(userAnswerId) ? null : userAnswerId.toString(),
+      // playerAnswerId: isNaN(userAnswerId) ? null : userAnswerId.toString(),
+      playerAnswerId: index,
       playerAnswerInSeconds: seconds,
       answerCorrect: (userAnswerId === correctAnswerId),
       questionId: this.currentQuestion.id,
