6759281e4f43532bb95ef86d64014a9423fae2c3
saloniss1345 <saloniss1345@gmail.com>
2018-06-25 19:25:37 +0530

    Added header validation in csv parser.

--- src/app/bulk/components/bulk-upload/bulk-upload.component.ts
+++ src/app/bulk/components/bulk-upload/bulk-upload.component.ts
@@ -97,74 +97,72 @@ export class BulkUploadComponent implements OnInit, OnDestroy {
     this.fileParseError = false;
     this.fileParseErrorMessage = '';
 
-    const parseOptions = {
-      'columns': columns => {
+    this.papa.parse(csvString, {
+      complete: (result) => {
+        const output = result.data;
+        const isHeaderValidate = false;
         const validColumns = ['Question', 'Option 1', 'Option 2', 'Option 3', 'Option 4', 'Answer Index', 'Tag 1', 'Tag 2', 'Tag 3',
           'Tag 4', 'Tag 5', 'Tag 6', 'Tag 7', 'Tag 8', 'Tag 9'];
-        if (validColumns.join(',') === columns.join(',')) {
-          return columns;
-        } else {
+        if (validColumns.toString() !== result.meta.fields.toString()) {
           this.fileParseError = true;
           this.fileParseErrorMessage = 'File format is not correct, must be in CSV format, must not have missing or wrong column order.';
           return '';
         }
-      },
-      'skip_empty_lines': true
-    };
-    this.papa.parse(csvString, {
-      complete: (result) => {
-        const output = result.data;
-        if (output !== undefined && output !== '') {
-          this.questions =
-            output.map(element => {
-              const question: Question = new Question();
-              question.questionText = element['Question'];
-              question.answers = [
-                { 'id': 1, 'answerText': element['Option 1'], correct: false },
-                { 'id': 2, 'answerText': element['Option 2'], correct: false },
-                { 'id': 3, 'answerText': element['Option 3'], correct: false },
-                { 'id': 4, 'answerText': element['Option 4'], correct: false }
-              ]
+        if (!this.fileParseError) {
+          if (output !== undefined && output !== '') {
+            this.questions =
+              output.map(element => {
+                const question: Question = new Question();
+                question.questionText = element['Question'];
+                question.answers = [
+                  { 'id': 1, 'answerText': element['Option 1'], correct: false },
+                  { 'id': 2, 'answerText': element['Option 2'], correct: false },
+                  { 'id': 3, 'answerText': element['Option 3'], correct: false },
+                  { 'id': 4, 'answerText': element['Option 4'], correct: false }
+                ]
 
-              if (question.answers[element['Answer Index'] - 1] !== undefined) {
-                question.answers[element['Answer Index'] - 1].correct = true;
-              }
+                if (question.answers[element['Answer Index'] - 1] !== undefined) {
+                  question.answers[element['Answer Index'] - 1].correct = true;
+                }
 
-              for (let i = 1; i < 10; i++) {
-                if (element['Tag ' + i] && element['Tag ' + i] !== '') {
-                  if (this.isTagExist(element['Tag ' + i], question) === -1) {
-                    question.tags.push(element['Tag ' + i].trim());
+                for (let i = 1; i < 10; i++) {
+                  if (element['Tag ' + i] && element['Tag ' + i] !== '') {
+                    if (this.isTagExist(element['Tag ' + i], question) === -1) {
+                      question.tags.push(element['Tag ' + i].trim());
+                    }
                   }
                 }
-              }
 
-              question.published = false;
-              question.explanation = 'status - not approved';
-              question.status = QuestionStatus.PENDING;
-              question.created_uid = this.user.userId;
+                question.published = false;
+                question.explanation = 'status - not approved';
+                question.status = QuestionStatus.PENDING;
+                question.created_uid = this.user.userId;
 
-              if (!question.questionText || question.questionText.trim() === '') {
-                this.questionValidationError = true;
-                question.validationErrorMessages.push('Missing Question');
-              } else if (question.answers[0].answerText.trim() === '' || question.answers[1].answerText.trim() === '' ||
-                question.answers[2].answerText.trim() === '' || question.answers[3].answerText.trim() === '') {
-                this.questionValidationError = true;
-                question.validationErrorMessages.push('Missing Question Answer Options');
-              } else if (question.answers.filter(a => a.correct).length !== 1) {
-                this.questionValidationError = true;
-                question.validationErrorMessages.push('Must have exactly one correct answer');
-              } else if (question.answers.filter(a => !a.answerText || a.answerText.trim() === '').length > 0) {
-                this.questionValidationError = true;
-                question.validationErrorMessages.push('Missing Answer');
-              } else if (question.tags.length < 3) {
-                this.questionValidationError = true;
-                question.validationErrorMessages.push('Atleast 3 tags required');
-              }
+                if (!question.questionText || question.questionText.trim() === '') {
+                  this.questionValidationError = true;
+                  question.validationErrorMessages.push('Missing Question');
+                } else if (question.answers[0].answerText.trim() === '' || question.answers[1].answerText.trim() === '' ||
+                  question.answers[2].answerText.trim() === '' || question.answers[3].answerText.trim() === '') {
+                  this.questionValidationError = true;
+                  question.validationErrorMessages.push('Missing Question Answer Options');
+                } else if (question.answers.filter(a => a.correct).length !== 1) {
+                  this.questionValidationError = true;
+                  question.validationErrorMessages.push('Must have exactly one correct answer');
+                } else if (question.answers.filter(a => !a.answerText || a.answerText.trim() === '').length > 0) {
+                  this.questionValidationError = true;
+                  question.validationErrorMessages.push('Missing Answer');
+                } else if (question.tags.length < 3) {
+                  this.questionValidationError = true;
+                  question.validationErrorMessages.push('Atleast 3 tags required');
+                }
 
-              return question;
-            });
-          this.bulkUploadFileInfo.uploaded = this.questions.length;
+                return question;
+              });
+            this.bulkUploadFileInfo.uploaded = this.questions.length;
+          }
         }
+
+
       },
       header: true,
       skipEmptyLines: true,
@@ -175,6 +173,59 @@ export class BulkUploadComponent implements OnInit, OnDestroy {
         }
       }
     });
+    // parse(csvString, parseOptions,
+    //   (error, output) => {
+    //     if (error) {
+    //       this.fileParseError = true;
+    //       this.fileParseErrorMessage = `File Parsing ${error}`;
+    //     }
+    //     if (output !== undefined && output !== '') {
+    //       this.questions =
+    //         output.map(element => {
+    //           const question: Question = new Question();
+    //           question.questionText = element['Question'];
+    //           question.answers = [
+    //             { 'id': 1, 'answerText': element['Option 1'], correct: false },
+    //             { 'id': 2, 'answerText': element['Option 2'], correct: false },
+    //             { 'id': 3, 'answerText': element['Option 3'], correct: false },
+    //             { 'id': 4, 'answerText': element['Option 4'], correct: false }
+    //           ]
+    //           if (question.answers[element['Answer Index'] - 1] !== undefined) {
+    //             question.answers[element['Answer Index'] - 1].correct = true;
+    //           }
+    //           for (let i = 1; i < 10; i++) {
+    //             if (element['Tag ' + i] && element['Tag ' + i] !== '') {
+    //               if (this.isTagExist(element['Tag ' + i], question) === -1) {
+    //                 question.tags.push(element['Tag ' + i].trim());
+    //               }
+    //             }
+    //           }
+    //           question.published = false;
+    //           question.explanation = 'status - not approved';
+    //           question.status = QuestionStatus.PENDING;
+    //           question.created_uid = this.user.userId;
+    //           if (!question.questionText || question.questionText.trim() === '') {
+    //             this.questionValidationError = true;
+    //             question.validationErrorMessages.push('Missing Question');
+    //           } else if (question.answers[0].answerText.trim() === '' || question.answers[1].answerText.trim() === '' ||
+    //             question.answers[2].answerText.trim() === '' || question.answers[3].answerText.trim() === '') {
+    //             this.questionValidationError = true;
+    //             question.validationErrorMessages.push('Missing Question Answer Options');
+    //           } else if (question.answers.filter(a => a.correct).length !== 1) {
+    //             this.questionValidationError = true;
+    //             question.validationErrorMessages.push('Must have exactly one correct answer');
+    //           } else if (question.answers.filter(a => !a.answerText || a.answerText.trim() === '').length > 0) {
+    //             this.questionValidationError = true;
+    //             question.validationErrorMessages.push('Missing Answer');
+    //           } else if (question.tags.length < 3) {
+    //             this.questionValidationError = true;
+    //             question.validationErrorMessages.push('Atleast 3 tags required');
+    //           }
+    //           return question;
+    //         });
+    //       this.bulkUploadFileInfo.uploaded = this.questions.length;
+    //     }
+    //   });
   }
 
   isTagExist(tag, question) {
