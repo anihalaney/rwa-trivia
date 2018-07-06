99b311b92e2089cc3d0cf3841c7a404777f8c94a
saloniss1345 <saloniss1345@gmail.com>
2018-07-06 14:45:05 +0530

    Use case related changes for news letter component.

---
+++ src/app/social/store/effects/social.effects.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed, async, inject } from '@angular/core/testing';
+import { provideMockActions } from '@ngrx/effects/testing';
+import { Observable } from 'rxjs';
+// import { AngularFire } from 'angularfire2';
+
+import { TEST_DATA } from '../../../testing/test.data';
+import { SocialEffects } from './social.effects';
+import { SocialActions } from '../actions';
+import { SocialService } from '../../../core/services/social.service';
+
+// describe('Effects: SocialEffects', () => {
+//     let effects: SocialEffects;
+//     let actions: Observable<any>;
+//     beforeEach(() => {
+//         TestBed.configureTestingModule({
+//             imports: [
+//                 // any modules needed
+//             ],
+//             providers: [
+//                 SocialEffects,
+//                 provideMockActions(() => actions),
+//                 // other providers
+//             ],
+//         });
+
+//         effects = TestBed.get(SocialEffects);
+//     });
+
+// });
+
