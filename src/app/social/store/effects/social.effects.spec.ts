d8ffb2aa8fc085e0e832ea8b6e91ad1d58b2653d
saloni <saloniss1345@gmail.com>
2018-07-09 20:12:30 +0530

    Created test cased for reducer and effect.

--- src/app/social/store/effects/social.effects.spec.ts
+++ src/app/social/store/effects/social.effects.spec.ts
@@ -3,12 +3,13 @@ import { provideMockActions } from '@ngrx/effects/testing';
 import { Observable } from 'rxjs';
 import { cold, hot, getTestScheduler } from 'jasmine-marbles';
 import { Actions } from '@ngrx/effects';
-
 import { TEST_DATA } from '../../../testing/test.data';
 import { SocialEffects } from './social.effects';
-import * as socialActions from '../../../social/store/actions';
 import { SocialService } from '../../../core/services/social.service';
-import { AddSubscriber, AddSubscriberSuccess, GetTotalSubscriber, GetTotalSubscriberSuccess, CheckSubscriptionStatus } from '../actions';
+import {
+    AddSubscriber, AddSubscriberSuccess, GetTotalSubscriber, GetTotalSubscriberSuccess, CheckSubscriptionStatus,
+    AddSubscriberError
+} from '../actions';
 import { Subscription, User, Subscribers } from '../../../model';
 import { StoreModule, Store } from '@ngrx/store';
 import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
@@ -16,14 +17,13 @@ import { AngularFirestore, AngularFirestoreModule } from 'angularfire2/firestore
 import { CONFIG } from '../../../../environments/environment';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthenticationProvider, AuthInterceptor } from '../../../../app/core/auth';
-export const firebaseConfig: FirebaseAppConfig = CONFIG.firebaseConfig;
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireStorageModule } from 'angularfire2/storage';
 import { UserService } from '../../../core/services';
-import { UserActions } from '../../../core/store/actions'
+import { UserActions } from '../../../core/store/actions';
 
-// import 'jest-preset-angular';
-// import './jestGlobalMocks';
+export const firebaseConfig: FirebaseAppConfig = CONFIG.firebaseConfig;
+
 
 describe('Effects: SocialEffects', () => {
     let effects: SocialEffects;
@@ -36,13 +36,15 @@ describe('Effects: SocialEffects', () => {
             providers: [
                 AngularFirestore,
                 SocialEffects,
-                SocialService,
+                {
+                    provide: SocialService,
+                    useValue: { getTotalSubscription: jest.fn(), checkSubscription: jest.fn(), saveSubscription: jest.fn() },
+                },
                 UserActions,
                 UserService,
                 provideMockActions(() => actions$),
                 {
                     provide: HTTP_INTERCEPTORS,
-                    useClass: AuthInterceptor,
                     multi: true,
                 }
             ],
@@ -58,72 +60,78 @@ describe('Effects: SocialEffects', () => {
         subscribers.count = 3;
         const action = new GetTotalSubscriber();
         const completion = new GetTotalSubscriberSuccess(subscribers);
-        actions$ = hot('--a-', { a: action });
+        actions$ = hot('-a---', { a: action });
         const response = cold('-a|', { a: subscribers });
         const expected = cold('--b', { b: completion });
-        // socialService.getTotalSubscription = jest.fn(() => response);
+        socialService.getTotalSubscription = jest.fn(() => response);
 
         expect(effects.getTotalSubscription$).toBeObservable(expected);
     });
 
-    // it('addSubscription for normal user', () => {
-    //     const obj = new Subscription();
-    //     obj.email = 'test@test.com';
-    //     const action = new AddSubscriber({ subscription: obj });
-    //     const completion = new CheckSubscriptionStatus(true);
-    //     actions$ = hot('--a-', { a: action });
-    //     const response = cold('-a|', { a: obj });
-    //     const expected = cold('--b', { b: completion });
-    //     socialService.checkSubscription = response;
+    it('addSubscription for normal user', () => {
+        const obj = new Subscription();
+        obj.email = 'test@test.com';
+        const action = new AddSubscriber({ subscription: obj });
+        const completion = new CheckSubscriptionStatus(true);
+        actions$ = hot('-a----', { a: action });
+        const response = cold('-a|', { a: obj });
+        const expected = cold('--b', { b: completion });
+        socialService.checkSubscription = jest.fn(() => response);
 
-    //     expect(effects.getTotalSubscription$).toBeObservable(expected);
-    // });
+        expect(effects.addSubscription$).toBeObservable(expected);
+    });
 
-    // it('addSubscription for normal user and save value', () => {
-    //     const obj = new Subscription();
-    //     obj.email = 'test@test.com';
-    //     const action = new AddSubscriber({ subscription: obj });
-    //     const completion = new CheckSubscriptionStatus(false);
-    //     actions$ = hot('--a-', { a: action });
-    //     const response = cold('-a|', { a: obj });
-    //     const expected = cold('--b', { b: completion });
-    //     socialService.checkSubscription = response;
-    //     expect(socialActions.CheckSubscriptionStatus).toBeFalsy();
-    //     socialService.saveSubscription(obj);
+    it('addSubscription for normal user throws Error', () => {
+        const obj = new Subscription();
+        obj.email = 'test@test.com';
+        const action = new AddSubscriber({ subscription: obj });
+        const completion = new AddSubscriberError('Error while saving Subscription');
+        const error = 'Error while saving Subscription';
 
-    //     expect(effects.getTotalSubscription$).toBeObservable(expected);
-    // });
+        actions$ = hot('-a----', { a: action });
+        const response = cold('-#|', {}, error);
+        const expected = cold('--b', { b: completion });
+        socialService.checkSubscription = jest.fn(() => response);
 
-    // it('addSubscription for logged in user', () => {
-    //     const user: User = { ...TEST_DATA.userList[0] };
-    //     const obj = new Subscription();
-    //     obj.email = user.email;
-    //     obj.userId = user.userId;
-    //     const action = new AddSubscriber({ subscription: obj });
-    //     const completion = new CheckSubscriptionStatus(true);
-    //     actions$ = hot('--a-', { a: action });
-    //     const response = cold('-a|', { a: obj });
-    //     const expected = cold('--b', { b: completion });
-    //     socialService.checkSubscription = response;
+        expect(effects.addSubscription$).toBeObservable(expected);
+    });
 
-    //     expect(effects.getTotalSubscription$).toBeObservable(expected);
-    // });
 
-    // it('addSubscription for  logged in and save value', () => {
-    //     const user: User = { ...TEST_DATA.userList[0] };
-    //     const obj = new Subscription();
-    //     obj.email = user.email;
-    //     obj.userId = user.userId;
-    //     const action = new AddSubscriber({ subscription: obj });
-    //     const completion = new CheckSubscriptionStatus(false);
-    //     actions$ = hot('--a-', { a: action });
-    //     const response = cold('-a|', { a: obj });
-    //     const expected = cold('--b', { b: completion });
-    //     socialService.checkSubscription = response;
-    //     expect(socialActions.CheckSubscriptionStatus).toBeFalsy();
-    //     socialService.saveSubscription(obj);
 
-    //     expect(effects.getTotalSubscription$).toBeObservable(expected);
-    // });
+    it('addSubscription for logged in user', () => {
+        const user: User = { ...TEST_DATA.userList[0] };
+        const obj = new Subscription();
+        obj.email = user.email;
+        obj.userId = user.userId;
+        const action = new AddSubscriber({ subscription: obj });
+        const completion = new CheckSubscriptionStatus(true);
+        actions$ = hot('-a---', { a: action });
+        const response = cold('-a|', { a: obj });
+        const expected = cold('--b', { b: completion });
+        socialService.checkSubscription = jest.fn(() => response);
+
+        expect(effects.addSubscription$).toBeObservable(expected);
+    });
+
+    it('addSubscription for logged in user throws Error', () => {
+        const user: User = { ...TEST_DATA.userList[0] };
+        const obj = new Subscription();
+        obj.email = user.email;
+        obj.userId = user.userId;
+        const action = new AddSubscriber({ subscription: obj });
+        const completion = new AddSubscriberError('Error while saving Subscription');
+        const error = 'Error while saving Subscription';
+
+
+        actions$ = hot('-a---', { a: action });
+        const response = cold('-#|', {}, error);
+        const expected = cold('--b', { b: completion });
+        socialService.checkSubscription = jest.fn(() => response);
+
+        expect(effects.addSubscription$).toBeObservable(expected);
+    });
+
+
+
 });
 
--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -1,1 +1,2 @@
 import 'jest-preset-angular';
+import './jestGlobalMocks';
