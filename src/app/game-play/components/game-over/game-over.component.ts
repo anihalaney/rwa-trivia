fc745046eabcc6f34b2b30dc844c13c2d57e97c5
Yugank Choksi <concent08@gmail.com>
2018-07-17 20:04:54 +0530

    Done changes relatd to share image on game over

--- src/app/game-play/components/game-over/game-over.component.ts
+++ src/app/game-play/components/game-over/game-over.component.ts
@@ -90,7 +90,7 @@ export class GameOverComponent implements OnInit {
       if (uploadTask != null) {
         if (uploadTask.task.snapshot.state === 'success') {
           const path = uploadTask.task.snapshot.metadata.fullPath.split('/');
-          const url = `${CONFIG.functionsUrl}/app/game/social/${this.user.userId}/${path[path.length - 1]}`;
+          const url = `https://${CONFIG.firebaseConfig.authDomain}/app/game/social/${this.user.userId}/${path[path.length - 1]}`;
           this.socialFeedData.share_status = true;
           this.socialFeedData.link = url;
           console.log('url---->', url);
