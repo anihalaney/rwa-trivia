6b17db07248b752f6f2d1db02a1156e61d7611f7
yugank <concent08@gmail.com>
2018-08-13 15:02:17 +0530

    Removed admin part from trivia app

--- src/app/bulk/components/index.ts
+++ src/app/bulk/components/index.ts
@@ -2,11 +2,14 @@ import { BulkDetailsComponent } from './bulk-details/bulk-details.component';
 import { BulkSummaryComponent } from './bulk-summary/bulk-summary.component';
 import { BulkSummaryTableComponent } from './bulk-summary/bulk-summary-table/bulk-summary-table.component';
 import { BulkSummaryQuestionComponent } from './bulk-summary/bulk-summary-question/bulk-summary-question.component';
+import { BulkUploadComponent } from './bulk-upload/bulk-upload.component';
+
 export {
   BulkDetailsComponent,
   BulkSummaryComponent,
   BulkSummaryTableComponent,
-  BulkSummaryQuestionComponent
+  BulkSummaryQuestionComponent,
+  BulkUploadComponent
 };
 
 export default [
@@ -14,4 +17,5 @@ export default [
   BulkSummaryComponent,
   BulkSummaryTableComponent,
   BulkSummaryQuestionComponent,
+  BulkUploadComponent
 ];
