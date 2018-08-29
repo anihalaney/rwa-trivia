b06026435080d3fff5271add755bf95c69064e4b
Yugank Choksi <concent08@gmail.com>
2018-08-29 16:27:41 +0530

    resolved issue related to save createdOn in elasticsearch

--- functions/db/firebase-functions.ts
+++ functions/db/firebase-functions.ts
@@ -40,10 +40,11 @@ exports.onQuestionWrite = functions.firestore.document('/questions/{questionId}'
     const data = change.after.data();
 
     if (data) {
-        // add or update
-        ESUtils.createOrUpdateIndex(ESUtils.QUESTIONS_INDEX, data.categoryIds['0'], data, context.params.questionId);
-
         const question: Question = data;
+        // add or update
+        ESUtils.createOrUpdateIndex(ESUtils.QUESTIONS_INDEX, data.categoryIds['0'], question, context.params.questionId);
+
+
         const userContributionStat: UserContributionStat = new UserContributionStat();
         userContributionStat.getUser(question.created_uid, UserStatConstants.initialContribution).then((userDictResults) => {
             console.log('updated user category stat');
