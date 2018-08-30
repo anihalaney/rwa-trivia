7e7cc448890ec229c033dbc2d720a272fa1ae4e8
yugank <concent08@gmail.com>
2018-08-30 12:35:47 +0530

    Done changes related to  generate analytics script for production env

--- projects/trivia/src/main.ts
+++ projects/trivia/src/main.ts
@@ -4,6 +4,29 @@ import { environment } from '../../shared-library/src/lib/environments/environme
 import { AppModule } from './app/app.module';
 
 if (environment.production) {
+  const script1 = document.createElement('script');
+  script1.src = 'https://www.googletagmanager.com/gtag/js?id=UA-122807814-1';
+  document.body.appendChild(script1);
+
+  const script2 = document.createElement('script');
+  script2.innerHTML = `(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+     }, i[r].l = 1 * new Date();
+     a = s.createElement(o),
+     m = s.getElementsByTagName(o)[0];
+     a.async = 1;
+     a.src = g;
+     m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+     ga('create', 'UA-122807814-1', 'auto');
+     ga('send', 'pageview');
+     window.dataLayer = window.dataLayer || [];
+     function gtag() { dataLayer.push(arguments); }
+       gtag('js', new Date());
+       gtag('config', 'UA-122807814-1');`;
+  document.body.appendChild(script2);
   enableProdMode();
 }
 document.addEventListener('DOMContentLoaded', () => {
