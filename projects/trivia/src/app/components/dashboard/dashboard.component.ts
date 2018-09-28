d1c3ac193d97fc1e8e9e0795fb4e6d8487b50929
jitendra Prajapati <jitendra.ashutec@gmail.com>
2018-09-28 15:54:33 +0530

    Display Auther below Question of the day in mobile

--- projects/trivia/src/app/components/dashboard/dashboard.component.ts
+++ projects/trivia/src/app/components/dashboard/dashboard.component.ts
@@ -13,111 +13,25 @@ import {
 } from '../../../../../shared-library/src/lib/shared/model';
 import { Utils, WindowRef } from '../../../../../shared-library/src/lib/core/services';
 import { AppState, appState, categoryDictionary } from '../../store';
+import { Dashboard } from './dashboard';
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss', './dashboard.scss']
 })
-export class DashboardComponent implements OnInit, OnDestroy {
-  user: User;
-  subs: Subscription[] = [];
-  users: User[];
-  questionOfTheDay$: Observable<Question>;
-  activeGames$: Observable<Game[]>;
-  userDict$: Observable<{ [key: string]: User }>;
-  gameSliceStartIndex: number;
-  gameSliceLastIndex: number;
-  gameInviteSliceStartIndex: number;
-  gameInviteSliceLastIndex: number;
-  now: Date;
-  greeting: string;
-  message: string;
-  activeGames: Game[];
-  showGames: boolean;
-  showNewsCard = true;
-  userDict: { [key: string]: User } = {};
-  missingCardCount = 0;
-  numbers = [];
-  gameInvites: Game[];
-  friendCount = 0;
-  randomPlayerCount = 0;
-  maxGameCardPerRow: number;
-  screenWidth: number;
+export class DashboardComponent extends Dashboard implements OnInit, OnDestroy  {
+  constructor(store: Store<AppState>,
+     questionActions: QuestionActions,
+     gameActions: GameActions,
+     userActions: UserActions,  windowRef: WindowRef,
+    @Inject(PLATFORM_ID)  platformId: Object){
 
-  constructor(private store: Store<AppState>,
-    private questionActions: QuestionActions,
-    private gameActions: GameActions,
-    private userActions: UserActions, private windowRef: WindowRef,
-    @Inject(PLATFORM_ID) private platformId: Object) {
-    this.questionOfTheDay$ = store.select(appState.coreState).pipe(select(s => s.questionOfTheDay));
-    this.activeGames$ = store.select(appState.coreState).pipe(select(s => s.activeGames));
-    this.userDict$ = store.select(appState.coreState).pipe(select(s => s.userDict));
-
-    this.userDict$.subscribe(userData => {
-      console.log('user data', userData);
-    })
-
-    this.subs.push(store.select(appState.coreState).pipe(select(s => s.user)).subscribe(user => {
-      this.user = user
-      if (user) {
-        this.user = user;
-        if (this.user.isSubscribed) {
-          this.showNewsCard = false;
-        }
-        // Load active Games
-        this.store.dispatch(this.gameActions.getActiveGames(user));
-        this.store.dispatch(new gameplayactions.LoadGameInvites(user.userId));
-
-      } else {
-        this.showNewsCard = true;
-      }
-    }));
-
-    this.subs.push(this.userDict$.subscribe(userDict => this.userDict = userDict));
-
-    this.subs.push(this.activeGames$.subscribe(games => {
-      this.activeGames = games;
-      if (games.length > 0) {
-        this.screenWidth = this.windowRef.nativeWindow.innerWidth;
-        this.checkCardCountPerRow();
-        this.activeGames.map(game => {
-          const playerIds = game.playerIds;
-          playerIds.map(playerId => {
-            if (playerId !== this.user.userId) {
-              if (this.userDict[playerId] === undefined) {
-                this.store.dispatch(this.userActions.loadOtherUserProfile(playerId));
-              }
-
-            }
-          });
-        });
-        this.showGames = true;
-      }
-    }));
-
-
-    this.gameSliceStartIndex = 0;
-    this.gameSliceLastIndex = 8;
-
-    this.subs.push(this.store.select(appState.gameplayState).pipe(select(s => s.gameInvites)).subscribe(iGames => {
-      this.gameInvites = iGames;
-      this.friendCount = 0;
-      this.randomPlayerCount = 0;
-      iGames.map(iGame => {
-        if (Number(iGame.gameOptions.opponentType) === OpponentType.Friend) {
-          this.friendCount++;
-        } else if (Number(iGame.gameOptions.opponentType) === OpponentType.Random) {
-          this.randomPlayerCount++;
-        }
-        this.store.dispatch(this.userActions.loadOtherUserProfile(iGame.playerIds[0]));
-      });
-    }))
-
-
-    this.gameInviteSliceStartIndex = 0;
-    this.gameInviteSliceLastIndex = 3;
-
+    super(store,
+         questionActions,
+        gameActions,
+        userActions, windowRef,
+         platformId);
   }
 
 
@@ -169,11 +83,6 @@ export class DashboardComponent implements OnInit, OnDestroy {
       }
     }
   }
-  // @HostListener('window: resize', ['$event'])
-  // onResize(event) {
-  //   this.screenWidth = event.target.innerWidth;
-  //   this.checkCardCountPerRow();
-  // }
 
   ngOnDestroy() {
     Utils.unsubscribe(this.subs);
