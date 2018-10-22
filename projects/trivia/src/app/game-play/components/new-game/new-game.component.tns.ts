e9cba994f31fa5277a0939d45496e8c1fae35550
jitendra Prajapati <jitendra.ashutec@gmail.com>
2018-10-22 14:44:34 +0530

    BW-337 #comment Added tag autocomplete feature for game options

--- projects/trivia/src/app/game-play/components/new-game/new-game.component.tns.ts
+++ projects/trivia/src/app/game-play/components/new-game/new-game.component.tns.ts
@@ -1,42 +1,125 @@
 import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { CheckBox } from 'nativescript-checkbox';
-import { RadListViewComponent } from 'nativescript-ui-listview/angular';
-import { ObservableArray } from 'tns-core-modules/data/observable-array/observable-array';
+import { Observable, Subscription } from 'rxjs';
+import * as gameplayactions from '../../store/actions';
+import * as useractions from '../../../user/store/actions';
+import { Store, select } from '@ngrx/store';
+import { GameActions } from 'shared-library/core/store/actions';
+import {
+  Category, GameOptions, GameMode, User, PlayerMode, OpponentType
+} from 'shared-library/shared/model';
+import { AppState, appState } from '../../../store';
+import { NewGame } from './new-game';
+import { Utils } from 'shared-library/core/services';
+import { ObservableArray } from 'tns-core-modules/data/observable-array';
+import { TokenModel } from 'nativescript-ui-autocomplete';
+import { RadAutoCompleteTextViewComponent } from 'nativescript-ui-autocomplete/angular';
 
-const data = [{id: 1, name: 'name 1', image: 'image'},
-              {id: 2, name: 'name 2', image: 'image'},
-              {id: 3, name: 'name 3', image: 'image'},
-              {id: 1, name: 'name 1', image: 'image'},
-              {id: 2, name: 'name 2', image: 'image'},
-              {id: 3, name: 'name 3', image: 'image'},
-              {id: 1, name: 'name 1', image: 'image'},
-              {id: 2, name: 'name 2', image: 'image'},
-              {id: 3, name: 'name 3', image: 'image'} ];
+
+const data = [{ id: 1, name: 'name 1', image: 'image' },
+{ id: 2, name: 'name 2', image: 'image' },
+{ id: 3, name: 'name 3', image: 'image' },
+{ id: 1, name: 'name 1', image: 'image' },
+{ id: 2, name: 'name 2', image: 'image' },
+{ id: 3, name: 'name 3', image: 'image' },
+{ id: 1, name: 'name 1', image: 'image' },
+{ id: 2, name: 'name 2', image: 'image' },
+{ id: 3, name: 'name 3', image: 'image' }];
 
 @Component({
   selector: 'new-game',
   templateUrl: './new-game.component.html',
   styleUrls: ['./new-game.component.scss']
 })
-export class NewGameComponent implements OnInit, OnDestroy {
+export class NewGameComponent extends NewGame implements OnInit, OnDestroy {
 
-  @ViewChild('contactListview') listViewComponent: RadListViewComponent;
   playerMode = 0;
   showSelectPlayer = false;
   showSelectCategory = false;
   showSelectTag = false;
   dataItem;
-  constructor() { }
+  categoriesObs: Observable<Category[]>;
+  categories: Category[];
+  subs: Subscription[] = [];
+  customTag: string;
+  private tagItems: ObservableArray<TokenModel>;
+  @ViewChild('autocomplete') autocomplete: RadAutoCompleteTextViewComponent;
+
+  constructor(public store: Store<AppState>,
+    public gameActions: GameActions,
+    public utils: Utils) {
+    super(store, utils);
+    this.initDataItems();
+  }
 
   ngOnInit() {
     this.dataItem = data;
-
-    // this.dataItem = ['s', 's', 'd'];
+    this.categories = [...this.categories.filter(c => c.requiredForGamePlay), ...this.categories.filter(c => !c.requiredForGamePlay)];
   }
 
   ngOnDestroy() { }
 
-  addTag() { }
+  addCustomTag() {
+    this.selectedTags.push(this.customTag);
+    this.customTag = '';
+    this.autocomplete.autoCompleteTextView.resetAutocomplete();
+  }
 
-  startGame() { }
+  startGame() {
+      this.gameOptions.tags = this.selectedTags;
+      this.gameOptions.categoryIds = this.gameActions.categoryIds;
+   }
+
+  selectCategory(category) {
+    category.isSelected = (!category.isSelected) ? true : false;
+    this.gameActions.categoryIds = this.categories.filter(c => c.requiredForGamePlay || c.isSelected).map(c => c.id);
+  }
+
+  getSelectedCatName() {
+    return this.categories.filter(c => c.requiredForGamePlay || c.isSelected).map(c => c.categoryName).join(', ');
+  }
+
+  getPlayerMode() {
+    return this.gameOptions.playerMode ? 'Single Player' : 'Two Player';
+  }
+
+  getGameMode() {
+    let gameMode = '';
+    if (this.gameOptions.playerMode === 1) {
+      switch (this.gameOptions.gameMode) {
+        case 0:
+          gameMode = 'Random';
+          break;
+        case 1:
+          gameMode = 'With Friend';
+          break;
+        case 2:
+          gameMode = 'With Computer';
+          break;
+      }
+    }
+    return gameMode;
+  }
+
+  get dataItems(): ObservableArray<TokenModel> {
+    return this.tagItems;
+  }
+
+  private initDataItems() {
+    this.tagItems = new ObservableArray<TokenModel>();
+
+    for (let i = 0; i < this.tags.length; i++) {
+      this.tagItems.push(new TokenModel(this.tags[i], undefined));
+    }
+  }
+
+
+  public onDidAutoComplete(args) {
+    this.selectedTags.push(args.text);
+  }
+
+  public onTextChanged(args) {
+    this.customTag = args.text;
+  }
 }
+
