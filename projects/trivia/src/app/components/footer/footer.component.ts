17b31b34e64f0791f68b98fda5876a0cb50687d8
yugank <concent08@gmail.com>
2018-10-25 16:07:29 +0530

    Added links of admin and home page

--- projects/trivia/src/app/components/footer/footer.component.ts
+++ projects/trivia/src/app/components/footer/footer.component.ts
@@ -1,21 +1,24 @@
-import { Component, Input, Output, EventEmitter, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnDestroy, } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { User } from '../../../../../shared-library/src/lib/shared/model';
+import { User, DashboardConstants } from '../../../../../shared-library/src/lib/shared/model';
+import { WindowRef } from 'shared-library/core/services';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit, OnDestroy {
   @Input() user: User;
   @Output() logoutClicked = new EventEmitter();
   @Output() loginClicked = new EventEmitter();
   blogUrl = 'https://bitwiser.io';
+  hostname: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private windowRef: WindowRef) {
+    this.hostname = `${DashboardConstants.HTTPS_PROTOCOL}${windowRef.nativeWindow.location.hostname}${DashboardConstants.SLASH}${DashboardConstants.ADMIN_ROUTE}`;
+  }
 
   ngOnInit() {
   }
