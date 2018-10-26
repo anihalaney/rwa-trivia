7f9295efde011d06539fddc1010745658d46269a
jitendra prajapati <jitendra.ashutec@gmail.com>
2018-10-26 14:50:56 +0530

    BW-346 #comment  Removed new game module from app component

--- projects/trivia/src/app/game-play/game-play.module.tns.ts
+++ projects/trivia/src/app/game-play/game-play.module.tns.ts
@@ -5,12 +5,12 @@ import { SharedModule } from 'shared-library/shared/shared.module';
 import { GamePlayRoutingModule } from './routing/game-play-routing.module';
 import { TNSCheckBoxModule } from 'nativescript-checkbox/angular';
 import { NativeScriptUIListViewModule } from 'nativescript-ui-listview/angular';
-import { NativeScriptUIAutoCompleteTextViewModule } from 'nativescript-ui-autocomplete/angular';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
 import {
   NewGameComponent, GameComponent, GameDialogComponent, GameQuestionComponent, GameOverComponent
 } from './components';
 import { effects, reducer } from './store';
+import { NativeScriptUIAutoCompleteTextViewModule } from 'nativescript-ui-autocomplete/angular';
 
 @NgModule({
   declarations: [
