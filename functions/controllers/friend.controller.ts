6c7d3ac792c238134909ab67fcbffd3780315506
Yugank Choksi <concent08@gmail.com>
2018-10-31 22:33:28 +0530

    Done changes related to restrict duplicate invitations

--- functions/controllers/friend.controller.ts
+++ functions/controllers/friend.controller.ts
@@ -9,9 +9,31 @@ exports.createFriends = (req, res) => {
     const userId = req.body.userId;
     const email = req.body.email;
 
+
     const makeFriends: MakeFriends = new MakeFriends(token, userId, email);
     makeFriends.validateToken().then((invitee) => {
         console.log('invitee', invitee);
         res.send({ created_uid: invitee });
+    });
+};
+
+
+/**
+ * createInvitations
+ * return string
+ */
+exports.createInvitations = (req, res) => {
+
+    const userId = req.body.userId;
+    const emails = req.body.emails;
+
+
+    if (!emails || !userId) {
+        res.status(400).send('Bad Request');
+    }
+
+    const makeFriends: MakeFriends = new MakeFriends(undefined, userId, undefined);
+    makeFriends.createInvitations(emails).then((status: string[]) => {
+        res.send({ messages: status.join('<br />') });
     });
 };
