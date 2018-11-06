82d98ccc3c2e761290d7b261b4bdc5610386199e
Milan Saraiya <milan@Milans-MacBook-Air-2.local>
2018-11-06 15:42:33 +0530

    BW-367 #comment Improved mobile dashbord UI in Ios

--- projects/trivia/src/app/components/dashboard/dashboard.ts
+++ projects/trivia/src/app/components/dashboard/dashboard.ts
@@ -80,6 +80,7 @@ export class Dashboard {
 
         store.select(appState.coreState).pipe(select(s => s.gameInvites)).subscribe(iGames => {
             this.gameInvites = iGames;
+            console.log('invite game', this.gameInvites);
             this.friendCount = 0;
             this.randomPlayerCount = 0;
             iGames.map(iGame => {
