a633a2866d739e01e5b6ce3ce93af1b0f1878fcc
jitendra prajapati <jitendra.ashutec@gmail.com>
2018-11-14 17:10:06 +0530

    BW-378 #comment fixed game play mobile ui in android

--- projects/trivia/src/app/components/dashboard/dashboard.component.tns.ts
+++ projects/trivia/src/app/components/dashboard/dashboard.component.tns.ts
@@ -16,7 +16,6 @@ import { RouterExtensions } from 'nativescript-angular/router';
 export class DashboardComponent extends Dashboard implements OnInit {
 
   gameStatus: any;
-  correctAnswerCount = 1;
   constructor(public store: Store<AppState>,
     questionActions: QuestionActions,
     gameActions: GameActions,
