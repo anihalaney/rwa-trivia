669346a51687e8977e5ae615c06f28ffd424a1e5
yugank1991 <concent08@gmail.com>
2018-11-21 14:49:17 +0530

    css related changes

--- projects/trivia/src/app/user/components/profile-settings/profile-settings.component.ts
+++ projects/trivia/src/app/user/components/profile-settings/profile-settings.component.ts
@@ -1,129 +1,157 @@
 import { Component, Input, OnDestroy, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 import { Store, select } from '@ngrx/store';
 import { AppState } from '../../../store';
 import { userState } from '../../../user/store';
 import { ProfileSettings } from './profile-settings';
-import { Utils, WindowRef } from 'shared-library/core/services';
+import { Utils } from 'shared-library/core/services';
 import { profileSettingsConstants } from 'shared-library/shared/model';
-import { ImageCropperComponent, CropperSettings } from 'ngx-img-cropper';
+import { ObservableArray } from 'tns-core-modules/data/observable-array';
+import { TokenModel } from 'nativescript-ui-autocomplete';
+import { RadAutoCompleteTextViewComponent } from 'nativescript-ui-autocomplete/angular';
+import { ImageAsset } from 'tns-core-modules/image-asset';
+import { ImageSource } from 'tns-core-modules/image-source';
+import { takePicture, requestPermissions, isAvailable } from 'nativescript-camera';
+import * as Toast from 'nativescript-toast';
 
 
 @Component({
   selector: 'profile-settings',
   templateUrl: './profile-settings.component.html',
-  styleUrls: ['./profile-settings.component.scss']
+  styleUrls: ['./profile-settings.component.css']
 })
 
 export class ProfileSettingsComponent extends ProfileSettings implements OnDestroy {
 
-  @ViewChild('cropper') cropper: ImageCropperComponent;
   // Properties
-  cropperSettings: CropperSettings;
-  notificationMsg: string;
-  errorStatus: boolean;
+  showSelectCategory = false;
+  showSelectTag = false;
+  dataItem;
+  customTag: string;
+  private tagItems: ObservableArray<TokenModel>;
+  SOCIAL_LABEL = 'CONNECT YOUR SOCIAL ACCOUNT';
+
+  public imageTaken: ImageAsset;
+  public saveToGallery = false;
+  public keepAspectRatio = true;
+  public width = 300;
+  public height = 300;
+
+
+  @ViewChild('autocomplete') autocomplete: RadAutoCompleteTextViewComponent;
 
   constructor(public fb: FormBuilder,
     public store: Store<AppState>,
-    private windowRef: WindowRef,
     public utils: Utils) {
 
     super(fb, store, utils);
-
-    this.setCropperSettings();
-    this.setNotificationMsg('', false, 0);
+    this.initDataItems();
 
     this.subs.push(this.store.select(userState).pipe(select(s => s.userProfileSaveStatus)).subscribe(status => {
       if (status === 'SUCCESS') {
-        this.setNotificationMsg('Profile Saved !', false, 100);
+        Toast.makeText('Profile is saved successfully').show();
+        this.toggleLoader(false);
       }
     }));
   }
 
-  setNotificationMsg(msg: string, flag: boolean, scrollPosition: number): void {
-    this.notificationMsg = msg;
-    this.errorStatus = flag;
-    if (this.windowRef.nativeWindow.scrollTo) {
-      this.windowRef.nativeWindow.scrollTo(0, scrollPosition);
-    }
+
+  get dataItems(): ObservableArray<TokenModel> {
+    return this.tagItems;
   }
 
+  onTakePhoto() {
+    const options = {
+      width: this.width,
+      height: this.height,
+      keepAspectRatio: this.keepAspectRatio,
+      saveToGallery: this.saveToGallery
+    };
 
-  private setCropperSettings() {
-    this.cropperSettings = new CropperSettings();
-    this.cropperSettings.noFileInput = true;
-    this.cropperSettings.width = 150;
-    this.cropperSettings.height = 140;
-    this.cropperSettings.croppedWidth = 150;
-    this.cropperSettings.croppedHeight = 140;
-    this.cropperSettings.canvasWidth = 300;
-    this.cropperSettings.canvasHeight = 280;
-    this.cropperSettings.minWidth = 10;
-    this.cropperSettings.minHeight = 10;
-    this.cropperSettings.rounded = false;
-    this.cropperSettings.keepAspect = false;
-    this.cropperSettings.cropperDrawSettings.strokeColor = 'rgba(255,255,255,1)';
-    this.cropperSettings.cropperDrawSettings.strokeWidth = 2;
-  }
-
-  onFileChange($event) {
-    this.validateImage($event.target.files);
-    if (!this.profileImageValidation) {
-      const image = new Image();
-      this.profileImageFile = $event.target.files[0];
-      const reader: FileReader = new FileReader();
-      reader.readAsDataURL(this.profileImageFile);
-      reader.onloadend = (loadEvent: any) => {
-        image.src = loadEvent.target.result;
-        this.user.originalImageUrl = image.src;
-        this.cropper.setImage(image);
-      };
+    if (isAvailable()) {
+      requestPermissions().then((status) => {
+        takePicture(options)
+          .then(imageAsset => {
+            this.imageTaken = imageAsset;
+            const source = new ImageSource();
+            source.fromAsset(imageAsset).then(imageSource => {
+              this.profileImage.image = `data:image/jpeg;base64,${imageSource.toBase64String('jpeg', 100)}`;
+              this.saveProfileImage();
+            });
+          }).catch(err => {
+            console.log(err.message);
+          });
+      });
     }
-  }
 
-  validateImage(fileList: FileList) {
-    if (fileList.length === 0) {
-      this.profileImageValidation = 'Please select Profile picture';
-    } else {
-      const file: File = fileList[0];
-      const fileName = file.name;
-      const fileSize = file.size;
-      const fileType = file.type;
-
-      if (fileSize > 2097152) {
-        this.profileImageValidation = 'Your uploaded Profile is not larger than 2 MB.';
-      } else {
-        if (fileType === 'image/jpeg' || fileType === 'image/jpg' || fileType === 'image/png' || fileType === 'image/gif') {
-          this.profileImageValidation = undefined;
-        } else {
-          this.profileImageValidation = 'Only PNG, GIF, JPG and JPEG Type Allow.';
-        }
-      }
-    }
   }
 
   saveProfileImage() {
-    if (!this.profileImageValidation) {
-      this.getUserFromFormValue(this.userForm.value);
-      this.assignImageValues();
-      this.saveUser(this.user);
-    }
+    this.getUserFromFormValue(this.userForm.value);
+    this.assignImageValues();
+    this.saveUser(this.user);
   }
 
   assignImageValues(): void {
-    const fileName = `${new Date().getTime()}-${this.profileImageFile.name}`;
+    const fileName = `${new Date().getTime()}.jpg`;
     this.user.profilePicture = fileName;
+    this.user.originalImageUrl = this.profileImage.image;
     this.user.croppedImageUrl = this.profileImage.image;
-    this.user.imageType = this.profileImageFile.type;
-    this.profileImageFile = undefined;
+    this.user.imageType = 'image/jpeg';
     this.userForm.get('profilePicture').setValue(fileName);
     this.userForm.updateValueAndValidity();
+  }
+
+
+  addCustomTag() {
+    if (this.customTag && this.customTag !== '' && this.customTag.length !== 0) {
+      this.enteredTags.push(this.customTag);
+      this.customTag = '';
+      this.autocomplete.autoCompleteTextView.resetAutocomplete();
+    } else {
+      Toast.makeText('Please Enter the text in Tags !').show();
+    }
+
+  }
+
+  selectCategory(category) {
+    category.isSelected = (!category.isSelected) ? true : false;
+  }
+
+  private initDataItems() {
+    this.tagItems = new ObservableArray<TokenModel>();
+
+    for (let i = 0; i < this.tagsAutoComplete.length; i++) {
+      this.tagItems.push(new TokenModel(this.tagsAutoComplete[i], undefined));
+    }
+  }
+
+  public onDidAutoComplete(args) {
+    this.customTag = args.text;
+  }
+
+  public onTextChanged(args) {
+    this.customTag = args.text;
+  }
+
+  removeEnteredTag(tag) {
+    this.enteredTags = this.enteredTags.filter(t => t !== tag);
+  }
+
+  selectProfileOption(profileOption) {
+    this.user.profileSetting = profileOption;
+    this.userForm.controls['profileSetting'].setValue(profileOption);
+  }
+
+  selectLocationOption(locationOption) {
+    this.user.profileLocationSetting = locationOption;
+    this.userForm.controls['profileLocationSetting'].setValue(locationOption);
   }
 
   setBulkUploadRequest(checkStatus: boolean): void {
     const userForm = this.userForm.value;
     if (!userForm.name || !userForm.displayName || !userForm.location || !userForm.profilePicture) {
-      this.setNotificationMsg('Please complete profile settings for bulk upload request', true, 100);
+      Toast.makeText('Please complete profile settings for bulk upload request').show();
     } else {
       this.user.bulkUploadPermissionStatus = profileSettingsConstants.NONE;
       this.onSubmit();
@@ -131,52 +159,30 @@ export class ProfileSettingsComponent extends ProfileSettings implements OnDestr
 
   }
 
-  // tags start
-  // Event Handlers
-  addTag() {
-    const tag = this.userForm.get('tags').value;
-    if (tag) {
-      if (this.enteredTags.indexOf(tag) < 0) {
-        this.enteredTags.push(tag);
-      }
-      this.userForm.get('tags').setValue('');
-    }
-    this.setTagsArray();
-  }
-
-  removeEnteredTag(tag) {
-    this.enteredTags = this.enteredTags.filter(t => t !== tag);
-    this.setTagsArray();
-  }
-
-  setTagsArray() {
-    this.tagsArray.controls = [];
-    this.enteredTags.forEach(tag => this.tagsArray.push(new FormControl(tag)));
-  }
-  // tags end
 
   onSubmit() {
     // validations
     this.userForm.updateValueAndValidity();
 
     if (!this.profileImageFile && !this.user.profilePicture) {
-      this.setNotificationMsg('Please upload the profile picture', true, 100);
+      Toast.makeText('Please upload the profile picture').show();
       return;
     } else if (this.profileImageFile) {
       this.assignImageValues();
     }
 
-
     if (this.userForm.invalid) {
-      this.setNotificationMsg('Please fill the mandatory fields', true, 100);
+      Toast.makeText('Please fill the mandatory fields').show();
       return;
     }
 
+
     // get user object from the forms
     this.getUserFromFormValue(this.userForm.value);
+    this.user.categoryIds = this.userCategories.filter(c => c.isSelected).map(c => c.id);
     // call saveUser
     this.saveUser(this.user);
-    this.setNotificationMsg('', false, 0);
+
   }
 
   ngOnDestroy() {
