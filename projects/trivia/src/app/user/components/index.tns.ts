661a32f930aa18a6685b591bb6944497994e07f6
Yugank Choksi <concent08@gmail.com>
2018-11-29 19:25:28 +0530

    Done changes related to integrate reason display screen

--- projects/trivia/src/app/user/components/index.tns.ts
+++ projects/trivia/src/app/user/components/index.tns.ts
@@ -6,6 +6,7 @@ import { ProfileSettingsComponent } from './profile-settings/profile-settings.co
 import { QuestionAddUpdateComponent } from './question-add-update/question-add-update.component';
 import { MyQuestionsComponent } from './my-questions/my-questions.component';
 
+
 export {
     GameCardComponent,
     GameInviteComponent,
