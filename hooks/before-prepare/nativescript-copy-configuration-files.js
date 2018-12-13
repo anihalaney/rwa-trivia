01c8fd37414e77cdf71785df87d64ddc8bde78bc
yugank <concent08@gmail.com>
2018-12-13 20:19:26 +0530

    configuration related changes

---
+++ hooks/before-prepare/nativescript-copy-configuration-files.js
@@ -0,0 +1,74 @@
+
+var path = require("path");
+var fs = require("fs");
+
+module.exports = function ($logger, $projectData, hookArgs) {
+
+    return new Promise(function (resolve, reject) {
+
+        /* Decide whether to prepare for dev or prod environment */
+
+        var isReleaseBuild = (hookArgs.appFilesUpdaterOptions && hookArgs.appFilesUpdaterOptions.release) ? true : false;
+        var validProdEnvs = ['prod', 'production'];
+        var isProdEnv = false; // building with --env.prod or --env.production flag
+
+        if (hookArgs.platformSpecificData.env) {
+            Object.keys(hookArgs.platformSpecificData.env).forEach((key) => {
+                if (validProdEnvs.indexOf(key) > -1) { isProdEnv = true; }
+            });
+        }
+
+        var buildType = isReleaseBuild || isProdEnv ? 'production' : 'development';
+
+
+        /* Handle preparing of Android xml files */
+
+        if (hookArgs.platform.toLowerCase() === 'android') {
+
+            // Replace facebooklogin.xml files with dev and prod environment
+
+            var destinationFacebookLoginXml = path.join($projectData.appResourcesDirectoryPath, "Android", "values", "facebooklogin.xml");
+            var destinationFacebookLoginXmlAlt = path.join($projectData.appResourcesDirectoryPath, "Android", "values", "facebooklogin.xml");
+            var sourceFacebookLoginXml = path.join($projectData.projectDir, "configurations", "android", "facebooklogin.xml");
+            var sourceFacebookLoginXmlProd = path.join($projectData.projectDir, "configurations", "android", "facebooklogin.prod.xml");
+            var sourceFacebookLoginXmlDev = path.join($projectData.projectDir, "configurations", "android", "facebooklogin.dev.xml");
+
+            // ensure we have both dev/prod versions so we never overwrite singlular facebooklogin.xml
+            if (fs.existsSync(sourceFacebookLoginXmlProd) && fs.existsSync(sourceFacebookLoginXmlDev)) {
+                if (buildType === 'production') { sourceFacebookLoginXml = sourceFacebookLoginXmlProd; } // use prod version
+                else { sourceFacebookLoginXml = sourceFacebookLoginXmlDev; } // use dev version
+            }
+
+            // copy correct version to destination
+            if (fs.existsSync(sourceFacebookLoginXml) && fs.existsSync(path.dirname(destinationFacebookLoginXml))) {
+                $logger.out("Copy " + sourceFacebookLoginXml + " to " + destinationFacebookLoginXml + ".");
+                fs.writeFileSync(destinationFacebookLoginXml, fs.readFileSync(sourceFacebookLoginXml));
+                resolve();
+            } else if (fs.existsSync(sourceFacebookLoginXml) && fs.existsSync(path.dirname(destinationFacebookLoginXmlAlt))) {
+                // NativeScript < 4 doesn't have the 'app' folder
+                $logger.out("Copy " + sourceFacebookLoginXml + " to " + destinationFacebookLoginXmlAlt + ".");
+                fs.writeFileSync(destinationFacebookLoginXmlAlt, fs.readFileSync(sourceFacebookLoginXml));
+                resolve();
+            } else {
+                $logger.warn("Unable to copy facebooklogin.xml.");
+                reject();
+            }
+        } else if (hookArgs.platform.toLowerCase() === 'ios') {  /* Handle preparing of Ios xml files */
+            // we have copied our Info.plist during before-checkForChanges hook, here we delete it to avoid changes in git
+            var destinationInfoPlist = path.join($projectData.appResourcesDirectoryPath, "iOS", "Info.plist");
+            var sourceInfoPlistProd = path.join($projectData.projectDir, "configurations", "ios", "Info.plist.prod");
+            var sourceInfoPlistDev = path.join($projectData.projectDir, "configurations", "ios", "Info.plist.dev");
+
+            // if we have both dev/prod versions, let's remove Info.plist in destination dir
+            if (fs.existsSync(sourceInfoPlistProd) && fs.existsSync(sourceInfoPlistDev)) {
+                if (fs.existsSync(destinationInfoPlist)) { fs.unlinkSync(destinationInfoPlist); }
+                resolve();
+            } else { // single Info.plist modus
+                $logger.warn("Unable to copy Info.plist.");
+                resolve();
+            }
+        } else {
+            resolve();
+        }
+    });
+};
