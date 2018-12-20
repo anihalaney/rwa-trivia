f05bb64762bcbc320e122317097cb78ba0570243
Yugank Choksi <yugankchoksi@Yuganks-Mac-mini.local>
2018-12-20 12:38:08 +0530

    environment files changes

--- projects/shared-library/src/lib/environments/environment.prod.ts
+++ projects/shared-library/src/lib/environments/environment.prod.ts
@@ -5,13 +5,12 @@ export const environment = {
 
 export const CONFIG: IConfig = {
   'firebaseConfig': {
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: ''
+    apiKey: 'AIzaSyDIEpabJv44Iu7go6M30T3WAF-GlSMcR7Y',
+    authDomain: 'bitwiser.io',
+    databaseURL: 'https://rwa-trivia.firebaseio.com',
+    projectId: 'rwa-trivia',
+    storageBucket: 'rwa-trivia.appspot.com',
+    messagingSenderId: '479350787602'
   },
-  'functionsUrl': ''
+  'functionsUrl': 'https://bitwiser.io'
 };
-
