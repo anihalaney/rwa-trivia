dc6f4a1f1f64923c4462104b635feb158ab698cf
Jitendra P <jitendra.ashutec@gmail.com>
2018-12-27 16:22:51 +0530

    BW-406 #comment dashboard min app, view more and reject on invitation.

--- projects/shared-library/src/lib/core/services/mobile/navigation.service.ts
+++ projects/shared-library/src/lib/core/services/mobile/navigation.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { RouterExtensions } from "nativescript-angular/router";
+import { Injectable, ComponentFactoryResolver } from '@angular/core';
+import { RouterExtensions } from 'nativescript-angular/router';
 import { Router } from '@angular/router';
 
 @Injectable()
@@ -16,17 +16,19 @@ export class NavigationService {
   }
 
   back() {
-    if (this.router.url === "/my/invite-friends" ||
-      this.router.url === "/my/questions" ||
-      this.router.url === "/my/recent-game" ||
+    console.log(this.router.url);
+    if (this.router.url === '/my/invite-friends' ||
+      this.router.url === '/my/questions' ||
+      this.router.url === '/login' ||
+      this.router.url === '/my/recent-game' ||
       this.router.url.includes('game-play') ||
       this.router.url.includes('/my/profile/') ||
       this.router.url.includes('/stats/leaderboard/')) {
-      this.routerExtensions.navigate(['/dashboard']);
+      this.routerExtensions.navigate(['/dashboard'], { clearHistory: true });
     } else if (
-      this.router.url === "/my/questions/add" ||
-      this.router.url === "/my/app-invite-friends-dialog"
-    ) { this.routerExtensions.back();
+      this.router.url === '/my/questions/add' ||
+      this.router.url === '/my/app-invite-friends-dialog') {
+      this.routerExtensions.back();
     }
   }
 }
--- projects/trivia/src/app/user/store/reducers/index.ts
+++ projects/trivia/src/app/user/store/reducers/index.ts
@@ -11,19 +11,15 @@ import { Observable } from 'rxjs';
 export * from './user.reducer';
 
 export interface UserState {
-    // userProfileSaveStatus: String;
     userPublishedQuestions: Question[];
     userUnpublishedQuestions: Question[];
     getGameResult: Game[];
-    // friendInvitations: Invitation[];
 }
 
 export const reducer: ActionReducerMap<UserState> = {
-    // userProfileSaveStatus: userProfileSaveStatus,
     userPublishedQuestions: userPublishedQuestions,
     userUnpublishedQuestions: userUnpublishedQuestions,
     getGameResult: getGameResult,
-    // friendInvitations: friendInvitations
 };
 
 export const userState = createFeatureSelector<UserState>('user');
