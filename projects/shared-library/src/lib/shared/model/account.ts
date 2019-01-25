653a399ff23af94bc38721611edb23fa86d61ce2
Yugank Choksi <yugankchoksi@Yuganks-Mac-mini.local>
2019-01-25 18:46:11 +0530

    Done changes related to move stat from user to account

--- projects/shared-library/src/lib/shared/model/account.ts
+++ projects/shared-library/src/lib/shared/model/account.ts
@@ -1,4 +1,18 @@
 export class Account {
     lives?: number;
     livesUpdatedAt?: number;
+    leaderBoardStats?: { [key: number]: number };
+    gamePlayed?: number;
+    categories?: number;
+    wins?: number;
+    badges?: number;
+    losses?: number;
+    avgAnsTime?: number;
+    contribution?: number;
+    id?: string;
+
+    constructor() {
+        this.leaderBoardStats = {};
+    }
 }
+
