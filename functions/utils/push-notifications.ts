71a6fc014fd358178096cf2bcfe976545d616e8e
Yugank Choksi <yugankchoksi@Yuganks-Mac-mini.local>
2019-02-01 18:07:59 +0530

    Done changes related to push notificaiton enhancement

--- functions/utils/push-notifications.ts
+++ functions/utils/push-notifications.ts
@@ -1,8 +1,9 @@
 const pushNotificationService = require('../services/push-notification.service');
 const pushNotificationUserService = require('../services/user.service');
 import {
-    User
+    User, Game, GameStatus, pushNotificationRouteConstants
 } from '../../projects/shared-library/src/lib/shared/model';
+
 
 export class PushNotification {
 
@@ -43,5 +44,58 @@ export class PushNotification {
             token: registrationToken
         };
         return pushNotificationService.sendPush(message);
+    }
+
+
+    public sendGamePlayPushNotifications(game: Game, currentTurnPlayerId: string) {
+        const data = { 'messageType': pushNotificationRouteConstants.GAME_PLAY, 'gameId': game.gameId };
+        pushNotificationUserService.getUserById(currentTurnPlayerId).then((user) => {
+            const dbUser: User = user.data();
+            console.log(game.GameStatus);
+            switch (game.GameStatus) {
+                case GameStatus.WAITING_FOR_NEXT_Q:
+                    // this.sendNotificationToDevices(game.nextTurnPlayerId, 'Bitwiser Game Play',
+                    //     `${dbUser.displayName} did not answer correctly. It's your turn to play!`, data)
+                    //     .then((result) => {
+                    //         console.log('result', result);
+                    //     }).catch((err) => {
+                    //         console.log('Notification Error: ', err);
+                    //     });
+                    console.log(`${dbUser.displayName} did not answer correctly. It's your turn to play!`);
+                    break;
+                case GameStatus.WAITING_FOR_RANDOM_PLAYER_INVITATION_ACCEPTANCE:
+                    // this.sendNotificationToDevices(game.nextTurnPlayerId, 'Bitwiser Game Play',
+                    //     `${dbUser.displayName} has invited you to a new game. It's your turn to play!`, data)
+                    //     .then((result) => {
+                    //         console.log('result', result);
+                    //     }).catch((err) => {
+                    //         console.log('Notification Error: ', err);
+                    //     });
+                    console.log(`${dbUser.displayName} has invited you to a new game. It's your turn to play!`);
+                    break;
+                case GameStatus.WAITING_FOR_FRIEND_INVITATION_ACCEPTANCE:
+                    // this.sendNotificationToDevices(game.nextTurnPlayerId, 'Bitwiser Game Play',
+                    //     `${dbUser.displayName} has invited you to a new game. It's your turn to play!`, data)
+                    //     .then((result) => {
+                    //         console.log('result', result);
+                    //     }).catch((err) => {
+                    //         console.log('Notification Error: ', err);
+                    //     });
+                    console.log(`${dbUser.displayName} has invited you to a new game. It's your turn to play!`);
+                    break;
+                case GameStatus.COMPLETED:
+                    const looserPlayerId = game.playerIds.filter((playerId) => playerId !== currentTurnPlayerId)[0];
+                    // this.sendNotificationToDevices(looserPlayerId, 'Bitwiser Game Play',
+                    //     `${dbUser.displayName} won the game.`, data)
+                    //     .then((result) => {
+                    //         console.log('result', result);
+                    //     }).catch((err) => {
+                    //         console.log('Notification Error: ', err);
+                    //     });
+                    console.log(`${dbUser.displayName} won the game.`);
+                    break;
+            }
+        });
+
     }
 }
