0ae784722149d83f30a8be9513c145296acec419
Priyanka Mavani <priyankamavani99@gmail.com>
2019-03-04 12:35:31 +0530

    BW-448 #comment Subscription route changes

--- functions/routes/subscription-routes.ts
+++ functions/routes/subscription-routes.ts
@@ -3,8 +3,6 @@ import * as express from 'express';
 import { SubscriptionController } from '../controllers/subscription.controller';
 const router = express.Router();
 
-const subscriptionController: SubscriptionController = new SubscriptionController();
-
-router.get('/count', subscriptionController.getSubscriptionCount);
+router.get('/count', SubscriptionController.getSubscriptionCount);
 
 module.exports = router;
