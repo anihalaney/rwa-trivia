5100a33c200f78fdcd1df012d12ccb829df58b29
Priyanka Mavani <priyankamavani99@gmail.com>
2019-03-05 16:23:19 +0530

    BW-448 #comment Util structure changes

--- functions/utils/system-stats-calculations.ts
+++ functions/utils/system-stats-calculations.ts
@@ -6,64 +6,53 @@ const statService = require('../services/stats.service');
 
 export class SystemStatsCalculations {
 
-    public generateSystemStats(): Promise<any> {
-        return statService.getSystemStats('system')
-            .then((systemStat) => {
-                const systemStatObj: SystemStats = (systemStat.data()) ? systemStat.data() : new SystemStats();
-                const systemStatPromises = [];
-                systemStatPromises.push(UserService.getUsers());
-                systemStatPromises.push(QuestionService.getAllQuestions());
-                systemStatPromises.push(GameService.getLiveGames());
-                systemStatPromises.push(GameService.getCompletedGames());
+    async generateSystemStats(): Promise<any> {
+        try {
+            const systemStat = await statService.getSystemStats('system');
+            const systemStatObj: SystemStats = (systemStat.data()) ? systemStat.data() : new SystemStats();
+            const systemStatPromises = [];
+            systemStatPromises.push(UserService.getUsers());
+            systemStatPromises.push(QuestionService.getAllQuestions());
+            systemStatPromises.push(GameService.getLiveGames());
+            systemStatPromises.push(GameService.getCompletedGames());
 
-                return Promise.all(systemStatPromises)
-                    .then((statResults) => {
-                        systemStatObj.total_users = statResults[0].size;
-                        systemStatObj.total_questions = statResults[1].size;
-                        systemStatObj.active_games = statResults[2].size;
-                        systemStatObj.game_played = statResults[3].size;
-                        return statService.setSystemStats('system', { ...systemStatObj }).then((status) => {
-                            return status;
-                        });
-                    });
-            });
+            const statResults = await Promise.all(systemStatPromises);
+            systemStatObj.total_users = statResults[0].size;
+            systemStatObj.total_questions = statResults[1].size;
+            systemStatObj.active_games = statResults[2].size;
+            systemStatObj.game_played = statResults[3].size;
+            return await  statService.setSystemStats('system', { ...systemStatObj });
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
     }
 
-    public updateSystemStats(entity: string): Promise<any> {
-        return statService.getSystemStats('system')
-            .then((systemStat) => {
-                const systemStatObj: SystemStats = (systemStat.data()) ? systemStat.data() : new SystemStats();
-                if (entity === 'total_users') {
-                    systemStatObj.total_users = (systemStatObj.total_users) ? systemStatObj.total_users + 1 : 1;
-                    return statService.setSystemStats('system', { ...systemStatObj }).then((status) => {
-                        return status;
-                    });
-                } else if (entity === 'total_questions') {
-                    systemStatObj.total_questions = (systemStatObj.total_questions)
-                        ? systemStatObj.total_questions + 1 : 1;
-                    return statService.setSystemStats('system', { ...systemStatObj }).then((status) => {
-                        return status;
-                    });
-                } else if (entity === 'active_games') {
-                    return GameService.getLiveGames()
-                        .then((active_games) => {
-                            systemStatObj.active_games = active_games.size;
-                            return statService.setSystemStats('system', { ...systemStatObj }).then((status) => {
-                                return status;
-                            });
-                        });
-                } else if (entity === 'game_played') {
-                    return GameService.getCompletedGames()
-                        .then((total_games) => {
-                            systemStatObj.game_played = total_games.size;
-                            return statService.setSystemStats('system', { ...systemStatObj }).then((status) => {
-                                return status;
-                            });
-                        });
-                }
+    async updateSystemStats(entity: string): Promise<any> {
+        try {
+            const systemStat = await statService.getSystemStats('system');
+            const systemStatObj: SystemStats = (systemStat.data()) ? systemStat.data() : new SystemStats();
+            if (entity === 'total_users') {
+                systemStatObj.total_users = (systemStatObj.total_users) ? systemStatObj.total_users + 1 : 1;
+                return statService.setSystemStats('system', { ...systemStatObj }).then((status) => status);
+            } else if (entity === 'total_questions') {
+                systemStatObj.total_questions = (systemStatObj.total_questions)
+                    ? systemStatObj.total_questions + 1 : 1;
+                return statService.setSystemStats('system', { ...systemStatObj }).then((status) => status);
+            } else if (entity === 'active_games') {
+                const active_games = await GameService.getLiveGames();
+                systemStatObj.active_games = active_games.size;
+                return await statService.setSystemStats('system', { ...systemStatObj });
+            } else if (entity === 'game_played') {
+                const total_games = await GameService.getCompletedGames();
+                systemStatObj.game_played = total_games.size;
+                return await statService.setSystemStats('system', { ...systemStatObj });
+            }
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
 
-
-            });
     }
 
 
