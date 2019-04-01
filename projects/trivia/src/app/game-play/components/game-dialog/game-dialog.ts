5684e2d5189062b3c217652435c1afe29a78af5d
jitendra prajapati <jitendra.ashutec@gmail.com>
2019-04-01 17:31:48 +0530

    BW-516 #comment resoved timeout issue in ios

--- projects/trivia/src/app/game-play/components/game-dialog/game-dialog.ts
+++ projects/trivia/src/app/game-play/components/game-dialog/game-dialog.ts
@@ -274,8 +274,8 @@ export class GameDialog {
           const addedOn = this.game.playerQnAs[this.game.playerQnAs.length - 1].addedOn;
           if (addedOn) {
             let timeDiff = this.currentQuestion.serverTimeQCreated - addedOn;
-            timeDiff = (timeDiff < 0 ) ? 0 : Math.round((timeDiff / 1000));
-            remainSecond =  this.MAX_TIME_IN_SECONDS - timeDiff;
+            timeDiff = (timeDiff < 0) ? 0 : Math.round((timeDiff / 1000));
+            remainSecond = this.MAX_TIME_IN_SECONDS - timeDiff;
           }
         }
       }
@@ -288,21 +288,20 @@ export class GameDialog {
           timer(1000, 1000).pipe(take(this.timer)).subscribe(t => {
             this.timer--;
             this.cd.markForCheck();
+            if (this.timer < 1) {
+              this.fillTimer();
+            }
           },
             null,
             () => {
               // disable all buttons
               if (this.currentQuestion) {
-                this.afterAnswer();
-                this.genQuestionComponent.fillTimer();
-                this.cd.markForCheck();
+               this.fillTimer();
               }
             });
       } else {
         setTimeout(() => {
-          this.afterAnswer();
-          this.genQuestionComponent.fillTimer();
-          this.cd.markForCheck();
+          this.fillTimer();
         }, 1000);
       }
     });
@@ -409,6 +408,12 @@ export class GameDialog {
     setTimeout(() => {
       this.cd.markForCheck();
     }, 0);
+  }
+
+  fillTimer() {
+    this.afterAnswer();
+    this.genQuestionComponent.fillTimer();
+    this.cd.markForCheck();
   }
 
   destroy() {
