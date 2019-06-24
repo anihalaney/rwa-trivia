052f0215ff851c41e8d5fc976e4c156c778ffb06
yugank1991 <concent08@gmail.com>
2019-06-24 19:02:47 +0530

    resolved push notificaiton issue

--- functions/services/push-notification.service.ts
+++ functions/services/push-notification.service.ts
@@ -1,5 +1,7 @@
 import admin from '../db/firebase.client';
 import { Utils } from '../utils/utils';
+import { User, interceptorConstants } from '../../projects/shared-library/src/lib/shared/model';
+import { UserService } from './user.service';
 
 export class PushNotificationService {
 
@@ -9,10 +11,18 @@ export class PushNotificationService {
      * sendPush
      * return PushResponse
      */
-    static async sendPush(message: any): Promise<any> {
+    static async sendPush(message: any, dbUser: User): Promise<any> {
         try {
             return await PushNotificationService.pushNotificationMessagingClient.send(message);
         } catch (error) {
+            if (error.code === interceptorConstants.ENTITY_NOT_FOUND) {
+                if (dbUser.androidPushTokens.indexOf(message.token) !== -1) {
+                    dbUser.androidPushTokens.splice(message.token, 1);
+                } else if (dbUser.iosPushTokens.indexOf(message.token) !== -1) {
+                    dbUser.iosPushTokens.splice(message.token, 1);
+                }
+                UserService.updateUser({ ...dbUser });
+            }
             return Utils.throwError(error);
         }
     }
