977574e77ab19625dd6fff6f45c035fd401537ae
niravgohil <nirav2018@gmail.com>
2019-07-04 19:40:27 +0530

    updated the changes

--- projects/trivia/src/scripts/google-analytics.js
+++ projects/trivia/src/scripts/google-analytics.js
@@ -8,12 +8,21 @@
         m = s.getElementsByTagName(o)[0];
     a.async = 1;
     a.src = g;
-    m.parentNode.insertBefore(a, m)
+    m.parentNode.insertBefore(a, m);
+    window.dataLayer = window.dataLayer || [];
 })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 ga('create', 'UA-122966274-1', 'auto');// add your tracking ID here.
 ga('send', 'pageview');
   
-window.dataLayer = window.dataLayer || [];
+
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
+(function (w, d, s, l, i) {
+    w[l] = w[l] || []; w[l].push({
+      'gtm.start':
+        new Date().getTime(), event: 'gtm.js'
+    }); var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+        'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-5RBD75M');
