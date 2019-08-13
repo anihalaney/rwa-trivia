15bcd3134cc51b0f0032a67f10386609370574b3
jitendraashutec <jitendra.ashutec@gmail.com>
2019-08-13 13:05:38 +0530

    BW-620 #comment select-category and first question flow

--- functions/services/account.service.ts
+++ functions/services/account.service.ts
@@ -352,7 +352,7 @@ export class AccountService {
             const appSetting = await AppSettings.Instance.getAppSettings();
             const bits = appSetting.first_question_bits;
             const accountData = await AccountService.getAccountById(userId);
-            accountData.bits = accountData.bits ? Utils.changeFieldValue(bits) : accountData.bits;
+            accountData.bits = accountData.bits ? Utils.changeFieldValue(bits) : bits;
             await AccountService.updateAccountData(accountData);
         } catch (error) {
             return Utils.throwError(error);
