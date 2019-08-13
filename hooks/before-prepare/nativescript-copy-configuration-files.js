bf75b82b5d73b3ecfd3295a60d2e1fbc17177662
yugank1991 <concent08@gmail.com>
2019-08-13 14:14:14 +0530

    updated the files

---
+++ hooks/before-prepare/nativescript-copy-configuration-files.js
@@ -0,0 +1,46 @@
+var nativeScriptConfig = require("./../../custom-hooks/nativescript-copy-configuration-files");
+module.exports = function ($logger, $projectData, hookArgs) {
+    return new Promise(function (resolve, reject) {
+        /* Decide whether to prepare for dev or prod environment */
+
+        var isReleaseBuild = (hookArgs.appFilesUpdaterOptions && hookArgs.appFilesUpdaterOptions.release) ? true : false;
+        if (isReleaseBuild) {
+            var validProdEnvs = ['prod', 'production'];
+            var isProdEnv = false; // building with --env.prod or --env.production flag
+
+            if (hookArgs.platformSpecificData.env) {
+                Object.keys(hookArgs.platformSpecificData.env).forEach((key) => {
+                    if (validProdEnvs.indexOf(key) > -1) { isProdEnv = true; }
+                });
+            }
+
+            var buildType = isProdEnv ? 'production' : 'development';
+
+            /* Handle preparing of Android xml files */
+
+            if (hookArgs.platform.toLowerCase() === 'android') {
+
+                nativeScriptConfig.copyAndroidConfig($projectData.appResourcesDirectoryPath,  // appResourcesDirPath
+                    $projectData.projectDir, // projectDir
+                    hookArgs.platformSpecificData.env.project, // projectName
+                    buildType, //buildType
+                    $logger);
+                resolve();
+
+            } else {
+                nativeScriptConfig.copyIosConfig($projectData.appResourcesDirectoryPath,  // appResourcesDirPath
+                    $projectData.projectDir, // projectDir
+                    hookArgs.platformSpecificData.env.project, // projectName
+                    buildType, //buildType
+                    $logger,
+                    hookArgs.platform.toLowerCase(),
+                    $projectData.platformsDir, // platformsDir
+                    isProdEnv);
+                resolve();
+            }
+
+        } else {
+            resolve();
+        }
+    });
+};
---
+++ hooks/before-watch/nativescript-copy-configuration-files.js
@@ -0,0 +1,45 @@
+var nativeScriptConfig = require("./../../custom-hooks/nativescript-copy-configuration-files");
+module.exports = function ($logger, $projectData, hookArgs) {
+    return new Promise(function (resolve, reject) {
+
+        /* Decide whether to prepare for dev or prod environment */
+        var env = (hookArgs.platformSpecificData || hookArgs.prepareData).env;
+        //   var isReleaseBuild = (hookArgs.appFilesUpdaterOptions && hookArgs.appFilesUpdaterOptions.release) ? true : false;
+
+        const platformFromHookArgs = hookArgs && (hookArgs.platform || (hookArgs.prepareData && hookArgs.prepareData.platform));
+        const platform = (platformFromHookArgs  || '').toLowerCase();
+        var validProdEnvs = ['prod', 'production'];
+        var isProdEnv = false; // building with --env.prod or --env.production flag
+
+        if (env) {
+            Object.keys(env).forEach((key) => {
+                if (validProdEnvs.indexOf(key) > -1) { isProdEnv = true; }
+            });
+        }
+
+        var buildType = isProdEnv ? 'production' : 'development';
+
+        /* Handle preparing of Android xml files */
+
+        if (platform === 'android') {
+            nativeScriptConfig.copyAndroidConfig($projectData.appResourcesDirectoryPath,  // appResourcesDirPath
+                $projectData.projectDir, // projectDir
+                env.project, // projectName
+                buildType, //buildType
+                $logger);
+            resolve();
+        } else {
+
+            nativeScriptConfig.copyIosConfig($projectData.appResourcesDirectoryPath,  // appResourcesDirPath
+                $projectData.projectDir, // projectDir
+                env.project, // projectName
+                buildType, //buildType
+                $logger,
+                platform,
+                $projectData.platformsDir, // platformsDir
+                isProdEnv);
+            resolve();
+
+        }
+    });
+};
