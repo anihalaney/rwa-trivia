a78975e9b5e79b819cb0869579e41cd3f84813aa
jitendraashutec <jitendra.ashutec@gmail.com>
2019-10-14 12:24:06 +0530

    BW-669 #comment after signup flow design

--- projects/shared-library/src/lib/shared/components/check-display-name/check-display-name.component.ts
+++ projects/shared-library/src/lib/shared/components/check-display-name/check-display-name.component.ts
@@ -17,6 +17,7 @@ import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 export class CheckDisplayNameComponent implements ControlValueAccessor {
   @Input() placeholder;
   @Input() hint;
+  @Input() class;
   myValue: any = null;
   disabled: boolean;
   propagateChange = (_: any) => { };
