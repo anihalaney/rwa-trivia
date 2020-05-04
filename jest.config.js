5dedb1c73e2f3b66fa56bdb78371fd8209840921
Priyanka Mavani <priyankamavani99@gmail.com>
2020-05-04 14:54:07 +0530

    BW-755 #comment add jest-stare as dev dependency

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 module.exports = {
+    collectCoverage: true,
     coverageReporters: ['html', 'text-summary'],
+    coverageDirectory: '<rootDir>/test/coverage',
+    reporters: ['default', ['jest-stare', {
+        "resultDir": "test/result",
+        "coverageLink": "../coverage/index.html"
+    }]],
     moduleNameMapper: {
-        "^shared-library/(.*)$": "<rootDir>/projects/shared-library/src/lib/$1"
+        '^shared-library/(.*)$': '<rootDir>/projects/shared-library/src/lib/$1'
     },
     testPathIgnorePatterns: ['/node_modules/', '/integration/'],
-    collectCoverage: true,
-    coverageDirectory: "<rootDir>/coverage",
   };
 
