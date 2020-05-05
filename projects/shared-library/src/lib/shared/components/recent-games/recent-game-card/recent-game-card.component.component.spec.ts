c4dc8bf66289d2e71d08a2b0d581a6d7cd49934f
jitendraashutec <jitendra.ashutec@gmail.com>
2020-05-05 16:58:47 +0530

    BW-756 #comment created recent game card componenet

--- projects/shared-library/src/lib/shared/components/recent-games/recent-game-card/recent-game-card.component.component.spec.ts
+++ projects/shared-library/src/lib/shared/components/recent-games/recent-game-card/recent-game-card.component.component.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RecentGamesComponent } from './recent-games.component';
+import { RecentGameCardComponent } from './recent-game-card.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { StoreModule, MemoizedSelector, Store } from '@ngrx/store';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { coreState, CoreState, UserActions } from 'shared-library/core/store';
 import { Utils, WindowRef } from 'shared-library/core/services';
 import { MatSnackBarModule } from '@angular/material';
-import { AppState } from '../../../../../../trivia/src/app/store';
-import { RecentGameCardComponent } from './recent-game-card/recent-game-card.component';
+import { AppState } from '../../../../../../../trivia/src/app/store';
 import { TEST_DATA } from 'shared-library/testing/test.data';
+import { UserCardComponent } from 'shared-library/shared/components';
 
-describe('RecentGamesComponent', () => {
-  let component: RecentGamesComponent;
-  let fixture: ComponentFixture<RecentGamesComponent>;
+describe('RecentGameCardComponent', () => {
+  let component: RecentGameCardComponent;
+  let fixture: ComponentFixture<RecentGameCardComponent>;
   let mockStore: MockStore<AppState>;
   let spy: any;
   let mockCoreSelector: MemoizedSelector<CoreState, Partial<CoreState>>;
@@ -36,19 +36,22 @@ describe('RecentGamesComponent', () => {
         UserActions,
         Utils,
         WindowRef,],
-      declarations: [RecentGamesComponent, RecentGameCardComponent]
+      declarations: [RecentGameCardComponent, UserCardComponent]
     });
 
-    fixture = TestBed.createComponent(RecentGamesComponent);
+    fixture = TestBed.createComponent(RecentGameCardComponent);
     mockStore = TestBed.get(Store);
     component = fixture.componentInstance;
+
+    component.user = TEST_DATA.userList[0];
+
     mockCoreSelector = mockStore.overrideSelector<CoreState, Partial<CoreState>>(coreState, {});
     spy = spyOn(mockStore, 'dispatch');
     fixture.detectChanges();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RecentGamesComponent);
+    fixture = TestBed.createComponent(RecentGameCardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
