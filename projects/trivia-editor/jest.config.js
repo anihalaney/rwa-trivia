d80ffae72f72e1476349c094d519f2cf6f157d0a
Milan Saraiya <milan@ashutec.com>
2020-05-07 18:51:08 +0530

    Configured code coverage and test result files per project to avoid overwrites

---
+++ projects/trivia-editor/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    coverageDirectory: '<rootDir>/test/coverage/trivia-editor',
+    reporters: ['default', ['jest-stare', {
+        "resultDir": "test/result/trivia-editor",
+        "coverageLink": "./../coverage/trivia-editor/index.html"
+    }]],
+  };
+
