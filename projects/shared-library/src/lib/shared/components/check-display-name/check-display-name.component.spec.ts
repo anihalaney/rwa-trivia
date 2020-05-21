43d5ac6e9302916fb6c2feccbfff0c7a552089cd
jitendraashutec <jitendra.ashutec@gmail.com>
2020-05-21 15:26:45 +0530

    Test-cases added for Check Display Name Component

---
+++ projects/shared-library/src/lib/shared/components/check-display-name/check-display-name.component.spec.ts
@@ -0,0 +1,67 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CheckDisplayNameComponent } from './check-display-name.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Utils, WindowRef } from 'shared-library/core/services';
+import { MatSnackBarModule } from '@angular/material';
+
+describe('CheckDisplayNameComponent', () => {
+  let component: CheckDisplayNameComponent;
+  let fixture: ComponentFixture<CheckDisplayNameComponent>;
+  let spy: any;
+  const event = 'new text change';
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CheckDisplayNameComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [MatSnackBarModule],
+      providers: [WindowRef,
+        Utils]
+    });
+
+    fixture = TestBed.createComponent(CheckDisplayNameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Initial value of the myValue should be falsy and propagateChange should be truthy', () => {
+    expect(component.propagateChange).toBeTruthy();
+    expect(component.myValue).toBeFalsy();
+  });
+
+  it(`show disabled if component is called from other than profile page `, () => {
+    component.isProfilePage = false;
+    component.ngOnInit();
+    expect(component.disabled).toEqual(false);
+  });
+
+  it(`call to writeValue function should set the input text`, () => {
+    spy = spyOn(component, 'writeValue');
+    component.writeValue(event);
+    expect(component.myValue).toEqual(event);
+  });
+
+  it(`call to onTextChange function should emit the propagateChange event`, () => {
+    spy = spyOn(component, 'propagateChange').and.callThrough();
+    expect(spy);
+    component.onTextChange(event);
+    expect(component.propagateChange).toHaveBeenCalled();
+  });
+
+  it(`call to registerOnChange function should emit the propagateChange event`, () => {
+    spy = spyOn(component, 'propagateChange').and.callThrough();
+    expect(spy);
+    component.registerOnChange(event);
+    expect(component.propagateChange).toBe(event);
+  });
+
+
+  it(`call to setDisabledState and change the input disabled based on argument received`, () => {
+    component.setDisabledState(true);
+    expect(component.disabled).toEqual(true);
+  });
+});
