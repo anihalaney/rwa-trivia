436283fa72bdfa2b2a2a4904f0577048b7b15fb6
jitendraashutec <jitendra.ashutec@gmail.com>
2020-05-29 12:48:36 +0530

    added click triggerEventHandler and removed js code

--- projects/shared-library/src/lib/shared/directive/open-user-profile.directive.spec.ts
+++ projects/shared-library/src/lib/shared/directive/open-user-profile.directive.spec.ts
@@ -1,4 +1,6 @@
+
 import { TestBed, ComponentFixture, inject } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { Component, Directive, Input, HostListener, NO_ERRORS_SCHEMA } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
@@ -8,9 +10,9 @@ import { testData } from 'test/data';
 
 @Component({
   template: `<img [stlOpenUserProfile]="{userId: user.userId, redirectTo: 'otherUserProfile'}" src="sample_Image.jpg"
-        alt="User Profile Picture" id="imageId1"/>
+        alt="User Profile Picture" class="image1"/>
         <img [stlOpenUserProfile]="{userId: user.userId, redirectTo: 'userProfile'}" src="sample_Image.jpg"
-        alt="User Profile Picture" id="imageId2" />`
+        alt="User Profile Picture" class="image2" />`
 })
 
 class TestOpenUserProfileComponent {
@@ -55,13 +57,13 @@ describe('OpenUserProfileDirective', () => {
 
   it(`redirect to other user's game profile if user click on other user's user card`, inject([Router], (mockRouter: Router) => {
     spy = spyOn(mockRouter, 'navigate');
-    document.getElementById('imageId1').click();
+    fixture.debugElement.query(By.css('.image1')).triggerEventHandler('click', null);
     expect(mockRouter.navigate).toHaveBeenCalledWith([`/user/game-profile/${component.user.userId}`]);
   }));
 
   it(`redirect to own game profile if user click on own user card`, inject([Router], (mockRouter: Router) => {
     spy = spyOn(mockRouter, 'navigate');
-    document.getElementById('imageId2').click();
+    fixture.debugElement.query(By.css('.image2')).triggerEventHandler('click', null);
     expect(mockRouter.navigate).toHaveBeenCalledWith([`/user/my/game-profile/${component.user.userId}`]);
   }));
 
