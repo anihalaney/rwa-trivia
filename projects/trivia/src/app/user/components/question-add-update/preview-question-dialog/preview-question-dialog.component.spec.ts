178e0c2a4eb3b562cc488c5e03f07e2eb6893ef0
Priyanka Mavani <priyankamavani99@gmail.com>
2020-06-03 12:41:20 +0530

    BW-806 #comment changes for preview-question-dialog component

---
+++ projects/trivia/src/app/user/components/question-add-update/preview-question-dialog/preview-question-dialog.component.spec.ts
@@ -0,0 +1,59 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { PreviewQuestionDialogComponent } from './preview-question-dialog.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { MatSnackBarModule } from '@angular/material';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material';
+import { testData } from 'test/data';
+import { Game } from 'shared-library/shared/model';
+
+describe('PreviewQuestionDialogComponent', () => {
+
+    let component: PreviewQuestionDialogComponent;
+    let fixture: ComponentFixture<PreviewQuestionDialogComponent>;
+    const dbModel = Game.getViewModel(testData.games[0]);
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [PreviewQuestionDialogComponent],
+            schemas: [NO_ERRORS_SCHEMA],
+            providers: [
+                {
+                    provide: MatDialogRef,
+                    useValue: {}
+                },
+                {
+                    provide: MAT_DIALOG_DATA,
+                    useValue: {question: testData.questions.published[0]}
+                },
+            ],
+            imports: [ MatSnackBarModule ]
+        });
+
+    }));
+
+    beforeEach(() => {
+        // create component
+        fixture = TestBed.createComponent(PreviewQuestionDialogComponent);
+        component = fixture.debugElement.componentInstance;
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should set data for question', () => {
+        expect(component.question).toEqual(testData.questions.published[0]);
+    });
+
+    it('verify closeModel function should work correctly', () => {
+        component.dialogRef.close = jest.fn();
+        component.closeModel();
+        expect(component.dialogRef.close).toHaveBeenCalledTimes(1);
+    });
+
+    afterEach(() => {
+        fixture.destroy();
+    });
+
+});
