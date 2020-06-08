bed03c04e0f35a783ce985ece8671f1f994916fe
jitendraashutec <jitendra.ashutec@gmail.com>
2020-06-08 17:38:02 +0530

    BW-820 Test cases written for ui-state.reducer

--- projects/shared-library/src/lib/core/store/reducers/ui-state.reducer.ts
+++ projects/shared-library/src/lib/core/store/reducers/ui-state.reducer.ts
@@ -1,6 +1,3 @@
-import { Observable } from 'rxjs';
-import { Action } from '@ngrx/store';
-
 import { ActionWithPayload, UIStateActions } from '../actions';
 
 export function loginRedirectUrl(state: any = null, action: ActionWithPayload<string>): string {
@@ -10,7 +7,7 @@ export function loginRedirectUrl(state: any = null, action: ActionWithPayload<st
     default:
       return state;
   }
-};
+}
 
 export function resetPasswordLogs(state: any = [], action: ActionWithPayload<string[]>): string[] {
   switch (action.type) {
@@ -19,6 +16,4 @@ export function resetPasswordLogs(state: any = [], action: ActionWithPayload<str
     default:
       return state;
   }
-};
-
-
+}
