e7c9f6e4636a916e35e3e98ef5f4805b0ff0cfdf
jitendraashutec <jitendra.ashutec@gmail.com>
2020-08-28 07:14:44 +0530

    BW-866 #comment unit test for game-play effect

--- projects/trivia/src/app/game-play/store/effects/game-play.effects.spec.ts
+++ projects/trivia/src/app/game-play/store/effects/game-play.effects.spec.ts
@@ -1,7 +1,7 @@
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { GameService, Utils } from 'shared-library/core/services';
 import { TestBed, async } from '@angular/core/testing';
-import * as GamePlayActions from '../actions';
+import * as GamePlayActions from '../actions/game-play.actions';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Actions } from '@ngrx/effects';
 import { hot, cold } from 'jest-marbles';
@@ -10,7 +10,7 @@ import { User, Game, GameOptions, ReportQuestion, QuestionMetadata, RouterStateU
 import { GamePlayEffects } from './game-play.effects';
 import { StoreModule, MemoizedSelector, Store } from '@ngrx/store';
 import { provideMockStore, MockStore } from '@ngrx/store/testing';
-import { coreState, CoreState } from 'shared-library/core/store';
+import { coreState, CoreState, ActionWithPayload } from 'shared-library/core/store';
 import { RouterNavigationPayload, RouterNavigationAction, ROUTER_NAVIGATION } from '@ngrx/router-store';
 import { RoutesRecognized } from '@angular/router';
 import { GameActions } from 'shared-library/core/store/actions/game.actions';
@@ -24,7 +24,7 @@ describe('GamePlayEffects', () => {
   let mockStore: MockStore<CoreState>;
   let mockCoreSelector: MemoizedSelector<CoreState, Partial<CoreState>>;
   const user: User = testData.userList[0];
-
+  let spy: any;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [StoreModule.forRoot({})],
@@ -58,6 +58,7 @@ describe('GamePlayEffects', () => {
     utils = TestBed.get(Utils);
     actions$ = TestBed.get(Actions);
     mockStore.refreshState();
+    spy = spyOn(mockStore, 'dispatch');
   }));
 
   // loadGame2
@@ -85,7 +86,157 @@ describe('GamePlayEffects', () => {
       return response;
     });
     expect(effects.loadGame2$).toBeObservable(expected);
+
   });
+
+
+  // loadGame2 check RESET_CURRENT called or not
+  // it('loadGame2 with RESET_CURRENT', () => {
+
+  //   const game = Game.getViewModel(testData.games[0]);
+  //   const routerState: RouterStateUrl = { url: `/game-play/${game.gameId}`, queryParams: {}, params: {} };
+  //   const event: RoutesRecognized = new RoutesRecognized(1, `/game-play/${game.gameId}`, '', null);
+  //   const payload: RouterNavigationPayload<RouterStateUrl> = {
+  //     routerState,
+  //     event
+  //   };
+
+  //   const action: RouterNavigationAction<RouterStateUrl> = {
+  //     type: ROUTER_NAVIGATION,
+  //     payload
+  //   };
+
+  //   const completion = new GamePlayActions.LoadGameSuccess(game);
+
+  //   actions$ = hot('-a---', { a: action });
+  //   const response = cold('-a|', { a: game });
+  //   const expected = cold('--b', { b: completion });
+  //   gameService.getGame = jest.fn(() => {
+  //     return response;
+  //   });
+
+  //   spy.and.callFake((action: ActionWithPayload<string>) => {
+  //     expect(action.type).toEqual(GamePlayActions.GamePlayActionTypes.RESET_CURRENT);
+  //     expect(action.payload).toEqual(null);
+  //   });
+
+  //   effects.loadGame2$.subscribe(res => {
+  //     // console.log('resFFF', res);
+  //     console.log("CLLEED>>>>>", res);
+  //     expect(mockStore.dispatch).toHaveBeenCalled();
+  //   });
+  //   // console.log('called>>>', new GamePlayActions.ResetCurrentGame().type);
+
+  //   // expect(mockStore.dispatch).toHaveBeenCalled();
+
+  //   // expect(effects.loadGame2$).toBeObservable(expected);
+  //   // console.log('mock>>>', mockStore.dispatch);
+  //   // expect(mockStore.dispatch).toHaveBeenCalled();
+
+  // });
+
+
+  // loadGame2 with game-play/challenge
+  it('loadGame2 with game-play/challenge route it should not complete call effects', () => {
+
+    const routerState: RouterStateUrl = { url: `/game-play/challenge`, queryParams: {}, params: {} };
+    const event: RoutesRecognized = new RoutesRecognized(1, `/game-play/challenge`, '', null);
+    const payload: RouterNavigationPayload<RouterStateUrl> = {
+      routerState,
+      event
+    };
+
+    const action: RouterNavigationAction<RouterStateUrl> = {
+      type: ROUTER_NAVIGATION,
+      payload
+    };
+
+    actions$ = hot('-a--', { a: action });
+    const response = cold('-a#', { a: [] });
+    const expected = cold('----');
+    gameService.getGame = jest.fn(() => {
+      return response;
+    });
+    expect(effects.loadGame2$).toBeObservable(expected);
+
+  });
+
+  // loadGame2 with /game-play/play-game-with-random-user
+  it('loadGame2 with /game-play/play-game-with-random-user route it should not complete call effects', () => {
+
+    const routerState: RouterStateUrl = { url: `/game-play/play-game-with-random-user`, queryParams: {}, params: {} };
+    const event: RoutesRecognized = new RoutesRecognized(1, `/game-play/play-game-with-random-user`, '', null);
+    const payload: RouterNavigationPayload<RouterStateUrl> = {
+      routerState,
+      event
+    };
+
+    const action: RouterNavigationAction<RouterStateUrl> = {
+      type: ROUTER_NAVIGATION,
+      payload
+    };
+
+    actions$ = hot('-a--', { a: action });
+    const response = cold('-a#', { a: [] });
+    const expected = cold('----');
+    gameService.getGame = jest.fn(() => {
+      return response;
+    });
+    expect(effects.loadGame2$).toBeObservable(expected);
+
+  });
+
+
+  // loadGame2 with /game-play/play-game-with-friend
+  it('loadGame2 with /game-play/play-game-with-friend route it should not complete call effects', () => {
+
+    const routerState: RouterStateUrl = { url: `/game-play/play-game-with-friend`, queryParams: {}, params: {} };
+    const event: RoutesRecognized = new RoutesRecognized(1, `/game-play/play-game-with-friend`, '', null);
+    const payload: RouterNavigationPayload<RouterStateUrl> = {
+      routerState,
+      event
+    };
+
+    const action: RouterNavigationAction<RouterStateUrl> = {
+      type: ROUTER_NAVIGATION,
+      payload
+    };
+
+    actions$ = hot('-a--', { a: action });
+    const response = cold('-a#', { a: [] });
+    const expected = cold('----');
+    gameService.getGame = jest.fn(() => {
+      return response;
+    });
+    expect(effects.loadGame2$).toBeObservable(expected);
+
+  });
+
+  // loadGame2 with /game-play/game-option
+  it('loadGame2 with /game-play/game-option route it should not complete call effects', () => {
+
+    const routerState: RouterStateUrl = { url: `/game-play/game-option`, queryParams: {}, params: {} };
+    const event: RoutesRecognized = new RoutesRecognized(1, `/game-play/game-option`, '', null);
+    const payload: RouterNavigationPayload<RouterStateUrl> = {
+      routerState,
+      event
+    };
+
+    const action: RouterNavigationAction<RouterStateUrl> = {
+      type: ROUTER_NAVIGATION,
+      payload
+    };
+
+    actions$ = hot('-a--', { a: action });
+    const response = cold('-a#', { a: [] });
+    const expected = cold('----');
+    gameService.getGame = jest.fn(() => {
+      return response;
+    });
+    expect(effects.loadGame2$).toBeObservable(expected);
+
+  });
+
 
   // startNewGame throws Error
   it('Start New Game throws Error', () => {
